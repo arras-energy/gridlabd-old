003ff5279039ca9ec7857f5ca944d6c1eafa5f4c
David P. Chassin <dchassin@slac.stanford.edu>
2018-12-26 14:20:18 -0800

    More comprehensive validating test

--- gldcore/link/python/validate.py
+++ gldcore/link/python/validate.py
@@ -2,7 +2,45 @@ import sys
 assert(sys.version_info.major>2)
 
 import gridlabd
-gridlabd.command('-W')
-gridlabd.command('../../..')
-gridlabd.command('--validate')
-gridlabd.start('batch')
+gridlabd.command('validate.glm')
+gridlabd.command('-D')
+gridlabd.command('show_progress=FALSE')
+gridlabd.start('thread')
+#gridlabd.start('wait')
+
+import time
+time.sleep(1)
+clock = gridlabd.get_global('clock')
+print("global.clock = '%s'\n" % clock)
+value = gridlabd.get_value('my_test','x')
+print("my_test.x = '%s'\n"%value)
+gridlabd.save('running.json');
+
+gridlabd.pause()
+
+print("global.clock = %s\n" % gridlabd.get_global('clock'))
+value = gridlabd.get_value('my_test','x')
+print("my_test.x = '%s'\n"%value)
+
+print("global.clock = %s\n" % gridlabd.get_global('clock'))
+value = gridlabd.get_value('my_test','x')
+print("my_test.x = '%s'\n"%value)
+
+gridlabd.resume()
+
+time.sleep(1)
+
+print("global.clock = %s\n" % gridlabd.get_global('clock'))
+value = gridlabd.get_value('my_test','x')
+print("my_test.x = '%s'\n"%value)
+
+gridlabd.wait()
+
+print("global.clock = %s\n" % gridlabd.get_global('clock'))
+value = gridlabd.get_value('my_test','x')
+print("my_test.x = '%s'\n"%value)
+
+gridlabd.save('done.json');
+gridlabd.save('done.glm');
+
+
