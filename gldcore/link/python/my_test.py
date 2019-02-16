1f6a1d83fc417b623478d1c03ed7fe01c539472e
David P. Chassin <dchassin@slac.stanford.edu>
2019-02-15 21:08:26 -0800

    Made python test a little more instructive

--- gldcore/link/python/my_test.py
+++ gldcore/link/python/my_test.py
@@ -1,11 +1,16 @@
+my_variable = None
+
 def on_init(t) :
+	global my_variable
+	my_variable = gridlabd.get_global("my_test")
 	print("timestamp,x")
 	return True
 
-def on_sync(t) :
-	print("%s,%s"%(gridlabd.get_global("clock"),gridlabd.get_object("my_test")["x"]))
-	return gridlabd.NEVER
-
 def commit(obj,t) :
-	print("%s.commit(obj='%s',t=%d) ok" % (gridlabd.__name__,obj,t))
+	global my_variable
+	if my_variable == "yes" :
+		my_variable = "no"
+	else :
+		my_variable = "yes"
+	print("%s.commit(obj='%s',t=%d) ok, my_variable = %s" % (gridlabd.__name__,obj,t,my_variable))
 	return gridlabd.NEVER
--- gldcore/link/python/validate.py
+++ gldcore/link/python/validate.py
@@ -6,7 +6,7 @@ gridlabd.command('validate.glm')
 gridlabd.command('-D')
 gridlabd.command('suppress_repeat_messages=FALSE')
 gridlabd.command('--warn')
-gridlabd.start('wait')
+gridlabd.start('pause')
 gridlabd.save('done.json');
 gridlabd.save('done.glm');
 
