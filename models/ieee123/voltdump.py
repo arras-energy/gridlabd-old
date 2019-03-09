4197fd2558915f47e1a1d680c1b023a09d6923f5
David P. Chassin <dchassin@slac.stanford.edu>
2019-03-09 09:46:53 -0800

    Fix to output complete voltage history
    Output from voltdump.py is written to output/voltages.csv

--- models/ieee123/voltdump.py
+++ models/ieee123/voltdump.py
@@ -1,26 +1,42 @@
-import csv
-import pandas as pd
 import os 
+import csv
+import datetime
 
-#def commit_voltdump(obj,t):
-time_stamp = []
-column_names = []
-value_voltA_real = []
-value_voltB_real = []
-value_voltC_real = []
-value_voltA_imag = []
-value_voltB_imag = []
-value_voltC_imag = []
-column_names.append("Time")
+data = {}
+nodes = []
+timestamp = None
+timezone = "UTC"
+with open('output/volt_dump.csv', 'r') as dumpfile:
+	reader = csv.reader(dumpfile)
+	for row in reader:
+		if row[0].startswith("#") :
+			tpos = row[0].find(" at ")
+			if tpos > 0 :
+				timestamp = row[0][tpos+4:tpos+27]
+				timestamp = datetime.datetime.strptime(timestamp,"%Y-%m-%d %H:%M:%S %Z")
+				data[timestamp] = []
+				timezone = row[0][tpos+24:tpos+27]
+			header = []
+		elif not header :
+			header = row
+			assert(header==['node_name', 'voltA_real', 'voltA_imag', 'voltB_real', 'voltB_imag', 'voltC_real', 'voltC_imag'])
+		else :
+			try :
+				node = row[0]
+				A = complex(float(row[1]),float(row[2]))
+				B = complex(float(row[3]),float(row[4]))
+				C = complex(float(row[5]),float(row[6]))
+				if not node+"_A" in nodes :
+					nodes.extend([node+"_A",node+"_B",node+"_C"])
+				data[timestamp].extend([A,B,C])
+			except :
+				print("ERROR: ignored row '%s'" % row)
 
-#voltdump is a file with appended timstamps to the end of the file
-with open('output/volt_dump.csv', 'r') as textfile:
-	for row, val in enumerate(list(csv.reader(textfile))): 
-		if (str(val)).startswith("['#") :
-			time_stamp.append((str(val))[32:55])
-		elif row == 1 : 
-			continue 
-		else : 
-			column_names.append(val[0])
-print(time_stamp)
-
+with open('output/voltages.csv','w') as voltages:
+	writer = csv.writer(voltages)
+	writer.writerow(nodes)
+	for key,values in data.items() :
+		data = [key.strftime("%Y-%m-%dT%H:%M:%S%z")]
+		for value in values:
+			data.append("%g%+gj" % (value.real,value.imag))
+		writer.writerow(data)
