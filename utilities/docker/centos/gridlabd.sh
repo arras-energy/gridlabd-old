097407eddff22546c1a2b63bd6797de87b46a0c7
aivanova5 <ivanova.alyona5@gmail.com>
2019-03-14 07:43:22 -0700

    Edited KEEP_SOURCE if statement to include a default.

--- utilities/docker/centos/gridlabd.sh
+++ utilities/docker/centos/gridlabd.sh
@@ -127,7 +127,7 @@ mkdir -p /usr/local/var
 adduser -d /usr/local/var/gridlabd gridlabd
 
 # clean up
-if [ "$KEEP_SOURCE" == "yes" ]; then
+if [ "${KEEP_SOURCE:-no}" == "yes" ]; then
 	echo "Cleaning up source code"
 	rm -rf /usr/local/src/*
 fi
