e9837079bfde8b31e348325a75a63ff950982f04
aivanova5 <ivanova.alyona5@gmail.com>
2019-04-10 13:43:55 -0700

    Rebuilt voltdump.py to account for missing timestamps in files.

--- models/ieee123/voltdump.py
+++ models/ieee123/voltdump.py
@@ -54,6 +54,8 @@ with open('output/voltages.csv','w') as voltages:
 
 headers = ["Timestamp"]
 data = {}
+timestamp_common = []
+timestamp_current = []
 re_complex = re.compile("([+-][0-9]*\\.?[0-9]+|[+-][0-9]+.[0-9]+[eE][0-9]+)([+-][0-9]*\\.?[0-9]+|[+-][0-9]+.[0-9]+[eE][0-9]+)([ijdr])")
 def to_complex(s) :
 	r = re.split(re_complex,s)
@@ -73,20 +75,36 @@ def to_complex(s) :
 for filename in os.listdir("output") :
 	if filename.startswith("power_dump_") :
 		with open("output/"+filename,"r") as dumpfile :
-			print("Read %s..." % filename)
+			print("Timestamp Read %s..." % filename)
 			reader = csv.reader(dumpfile)
-			for row in reader:
+ 			for row in reader:
+ 				if '#' not in row[0][0] : 
+ 					timestamp_current.append(datetime.datetime.strptime(row[0],"%Y-%m-%d %H:%M:%S %Z"))
+			if not timestamp_common : 
+				timestamp_common = timestamp_current[:]
+				continue
+ 		timestamp_common = set(timestamp_common) & set(timestamp_current)
+ 		timestamp_current = []
+
+for filename in os.listdir("output") :
+	if filename.startswith("power_dump_") :
+		with open("output/"+filename,"r") as dumpfile :
+			print("Data Read %s..." % filename)
+ 			reader = csv.reader(dumpfile)
+ 			for row in reader :
+ 				if '#' not in row[0][0] :
+ 					timestamp = datetime.datetime.strptime(row[0],"%Y-%m-%d %H:%M:%S %Z")
+					if not timestamp in data.keys() and timestamp in list(timestamp_common) : 
+						data[timestamp] = []
+ 					if timestamp in list(timestamp_common) : 
+ 							data[timestamp].extend(list(map(lambda x:to_complex(x),row[1:])))
+ 					else : 
+ 						print("Timestamp '%s' unique to '%s'..." % (timestamp, filename))
 				if row[0][0] == '#' :
 					if row[0]=="# timestamp" :
 						headers.extend(row[1:])
 					continue
-				timestamp = datetime.datetime.strptime(row[0],"%Y-%m-%d %H:%M:%S %Z")
-				if not timestamp in data.keys() :
-					data[timestamp] = []
-				try :
-					data[timestamp].extend(list(map(lambda x:to_complex(x),row[1:])))
-				except:
-					print("%s: error parsing row '%s', values ignored" % (filename,row))
+
 
 with open("output/powers.csv","w") as powers:
 	print("Writing powers...")
@@ -95,5 +113,6 @@ with open("output/powers.csv","w") as powers:
 	for key,values in data.items() :
 		data = [key.strftime("%Y-%m-%dT%H:%M:%S%z")]
 		for value in values:
-			data.append("%g%+gj" % (value.real,value.imag))
+			#data.append("%g%+gj" % (value.real,value.imag))
+			data.append("%g%+gd" % (abs(value),(cmath.phase(value))*180/3.1415926))
 		writer.writerow(data)
