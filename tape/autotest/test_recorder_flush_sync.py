e97544ee341dd0d43c0c31bd8ce50591e2f5888c
Alyona Ivanova <Ivanova.alyona5@gmail.com>
2019-04-18 10:43:28 -0700

    Addressing validation errors  (#196)
    * Added curl to python setup.py
    
    * Fixed json file to not output methods.
    
    * Fixed recorder flush tests.
    
    * Updated test_recorder_flush_300.glm

--- tape/autotest/test_recorder_flush_sync.py
+++ tape/autotest/test_recorder_flush_sync.py
@@ -1,33 +1,40 @@
 import csv
 import time 
 import datetime
+import os
+import json
 
-with open('test_recorder_flush.csv', 'r') as textfile:
-	
-	temp_row = 0
-	first_row_flag = True
-	delta = 0
+json_exists = os.path.isfile('gridlabd.json')
 
-	for row in reversed(list(csv.reader(textfile))):
-		try:
-			time_col = datetime.datetime.strptime(row[0][:-3].rstrip(), "%Y-%m-%d %H:%M:%S")
-		except:
-			continue
+#csv_exists = os.path.isfile('test_recorder_flush.csv')
+if json_exists : 
+	with open("gridlabd.json", "r") as jsonfile : 
+		data = json.load(jsonfile)
+		clock = data["globals"]["clock"]
+		global_clock = datetime.datetime.strptime(clock["value"][:-3].rstrip(), "%Y-%m-%d %H:%M:%S")
+	#	print('global clock time : ', global_clock)
+		flush_duration = int(int(data["objects"]["recorder:1"]["flush"])/60) #converted to min
+		if flush_duration == 0 : 
+			flush_duration = 1
 
-		if first_row_flag:
-			temp_row = time_col
-			first_row_flag = False
-			continue
-
-		delta = temp_row-time_col		
-		
-
-		if delta == datetime.timedelta(minutes=1):
-			#print("success", delta)
-			exit(0)
-		else: 
-			#print("fail", delta)
+	with open('test_recorder_flush.csv', 'r') as textfile:
+		temp_row = 0
+		first_row_flag = True
+		delta = 0
+		if os.stat('test_recorder_flush.csv').st_size == 0 : #empty file
 			exit(1)
-		break
+		else : 
+			time_csv = list(csv.reader(textfile))[-1][0]
+	#		print('csv recorded time :  ',time_csv)
+			time_col = datetime.datetime.strptime(time_csv[:-3].rstrip(), "%Y-%m-%d %H:%M:%S")
+			delta = abs(global_clock - time_col)
+			if delta <= datetime.timedelta(minutes=flush_duration) : 
+				#print('success' , delta )
+				exit(0)
+			else : 
+				#print('fail', delta)
+				exit(1)
+			
+
 
 
