34c9a39998b578bccbe8de0e35bc1080f98c9313
Alyona Ivanova <Ivanova.alyona5@gmail.com>
2019-05-09 10:22:16 -0700

    Pip3 upgrade (#210)
    * Pip3 upgrade
    
    * Fixed git path reference in gridlabd.sh

--- utilities/docker/centos/system.sh
+++ utilities/docker/centos/system.sh
@@ -19,4 +19,6 @@ yum install which -y
 yum --disablerepo="*" --enablerepo="epel" install python36 -y
 yum install python36 python36-devel python36-pip python36-tkinter -y
 [ -f /usr/bin/python3 ] || ln -s /usr/bin/python3.6 /usr/bin/python3
+pip3 install --upgrade pip
+ln -s /usr/local/bin/pip3 /usr/bin/pip3
 pip3 install matplotlib
