56dab71efa39cbc1b094628ba336f919dd00d466
David P. Chassin <dchassin@slac.stanford.edu>
2019-06-04 09:40:58 -0700

    Update index.h

--- gldcore/index.h
+++ gldcore/index.h
@@ -1,11 +1,6 @@
-/* File: index.h 
- * Copyright (C) 2008, Battelle Memorial Institute
-
-	@file index.h
-	@addtogroup index
-	@ingroup core
-@{
- **/
+/*	File: index.h 
+ 	Copyright (C) 2008, Battelle Memorial Institute
+ */
 
 #ifndef _INDEX_H
 #define _INDEX_H
@@ -17,25 +12,60 @@
 #include "globals.h"
 #include "list.h"
 
-typedef struct s_index {
+/*	Structure: s_index
+	unsigned int id - the index id
+	GLLIST **ordinal - the list of ordinals
+	int first_ordinal - the first ordinal in the list
+	int last_ordinal - the last ordinal in the list
+	int last_used - the last ordinal in use
+	int first_used - the first ordinal in use
+ */
+struct s_index 
+{
 	unsigned int id;	/**< the index id */
 	GLLIST **ordinal;		/**< the list of ordinals */
 	int first_ordinal;	/**< the first ordinal in the list */
 	int last_ordinal;	/**< the last ordinal in the list */
 	int last_used;		/**< the last ordinal in use */
 	int first_used;		/**< the first ordinal in use */
-} INDEX;	/**< the index structure */
+};
+
+// Typedef: INDEX
+// See <s_index>
+typedef struct s_index INDEX;	/**< the index structure */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-INDEX *index_create(int first_ordinal, int last_ordinal);
-STATUS index_insert(INDEX *index, void *data, int ordinal);
-void index_shuffle(INDEX *index);
+// Function: index_create
+DEPRECATED INDEX *index_create(int first_ordinal, int last_ordinal);
+
+// Function: index_insert
+DEPRECATED STATUS index_insert(INDEX *index, void *data, int ordinal);
+
+// Function: index_shuffle
+DEPRECATED void index_shuffle(INDEX *index);
 
 #ifdef __cplusplus
 }
+
+class GldExec;
+
+// Class: GldIndex
+class GldIndex
+{
+private:
+	GldExec *exec;
+public:
+
+	// Constructor: GldIndex
+	GldIndex(GldExec *ptr);
+
+	// Destructor: ~GldIndex
+	~GldIndex(void);
+};
+
 #endif
 
 #endif
