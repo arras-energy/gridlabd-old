66ffa2bc5d4859cf623a010710b505a488d00db5
David P. Chassin <dchassin@slac.stanford.edu>
2019-07-09 09:51:27 +0900

    Added document to orderbook python module

--- transactive/orderbook.py
+++ transactive/orderbook.py
@@ -6,24 +6,29 @@ warning = True
 debug = False
 
 def print_message(msg,msgtype="MESSAGE"):
+	"""Print a message"""
 	print("%-19.19s [%s] %s" % (datetime.datetime.now(),msgtype,msg))
 
 def print_warning(msg):
+	"""Print a warning message (module.warning must be True)"""
 	if warning:
 		print_message(msg,msgtype="WARNING")
 
 def print_debug(msg):
+	"""Print a debug message (module.debug must be True)"""
 	if debug:
 		print_message(msg,msgtype="DEBUG")
 
 class orderbook:
-
-	def __init__(self,unit="MW",time="h"):
+	"""Implementation of orderbook"""
+	def __init__(self,unit="MW",time="h",currency="$"):
 		self.unit 	= unit
 		self.time	= time
+		self.currency = currency
 		self.reset()
 
 	def reset(self):
+		"""Reset the market to initial values"""
 		self.buy 	= []
 		self.sell 	= []
 		self.using = []
@@ -42,6 +47,7 @@ class orderbook:
 			})
 
 	def submit(self,order):
+		"""Submit an order to the market"""
 		if order.islimit():
 			if order.issell():
 				self.sell.append(order)
@@ -63,11 +69,12 @@ class orderbook:
 		return order
 
 	def clear(self):
+		"""Clear all matching limit orders"""
 		skip = 0
 		while len(self.buy) > skip and len(self.sell) > 0 and self.buy[skip].get_price() >= self.sell[0].get_price():
 			trade = min(self.buy[skip].get_quantity(),self.sell[0].get_quantity())
 			if not self.buy[skip].get_divisible() and trade < self.buy[skip].get_quantity():
-				print_warning("%s cannot buy %g from %s due to indivisible bid" % (repr(self.buy[skip]),trade,repr(self.sell[0])))
+				print_debug("%s cannot buy %g from %s due to indivisible bid" % (repr(self.buy[skip]),trade,repr(self.sell[0])))
 				skip += 1
 			else:
 				print_debug("%s buying %g from %s" % (repr(self.buy[skip]),trade,repr(self.sell[0])))
@@ -86,18 +93,21 @@ class orderbook:
 			
 
 	def ask(self,quantity,duration,price=None):
+		"""Place a sell order"""
 		if price is None:
 			return self.submit(order(order_type="SELLMARKET",quantity=quantity,duration=duration))
 		else:
 			return self.submit(order(order_type="SELLLIMIT",quantity=quantity,duration=duration,price=price))
 
 	def bid(self,quantity,duration,price=None):
+		"""Plase a buy order"""
 		if price is None:
 			return self.submit(order(order_type="BUYMARKET",quantity=quantity,duration=duration))
 		else:
 			return self.submit(order(order_type="BUYLIMIT",quantity=quantity,duration=duration,price=price))
 
 	def find_buy(self,order):
+		"""Find a buy for a sell market order"""
 		# TODO
 		return None
 
@@ -115,17 +125,19 @@ class orderbook:
 		return order
 
 	def find_sell(self,order):
+		"""Find a sell for a buy market order"""
 		# TODO
 		return None
 
 	def plot(self,using=None):
+		"""Plot the current orderbooks"""
 		if using is None:
 			using = self.using
 		else:
 			self.using = using
 		fig = plt.figure()
 		plt.ylabel("Quantity (%s)" % (self.unit))
-		plt.xlabel("Price ($/%s.%s)" % (self.unit,self.time))
+		plt.xlabel("Price (%s/%s.%s)" % (self.currency,self.unit,self.time))
 		sell = []
 		q = [0.0]
 		p = [0.0]
@@ -150,7 +162,7 @@ class orderbook:
 		return {"sell":sell,"buy":buy,"figure":fig,"plot":plt} 
 
 class ordertype(str):
-
+	"""Implementation of order type"""
 	def __new__(cls, ordertype):
 		if ordertype in ["CANCEL","BUYMARKET","SELLMARKET","BUYLIMIT","SELLLIMIT"]:
 			return str.__new__(cls, ordertype)
@@ -158,26 +170,32 @@ class ordertype(str):
 			raise Exception("ordertype('%s') is not valid" % (ordertype))
 
 	def iscancel(self):
+		"""Test if order is cancelled"""
 		return self == "CANCEL"
 
 	def ismarket(self):
+		"""Test if market order"""
 		return self == "BUYMARKET" or self == "SELLMARKET"
 
 	def islimit(self):
+		"""Test if limit order"""
 		return self == "BUYLIMIT" or self == "SELLLIMIT"
 
 	def isbuy(self):
+		"""Test if buy order"""
 		return self == "BUYMARKET" or self == "BUYLIMIT"
 
 	def issell(self):
+		"""Test if sell order"""
 		return self == "SELLMARKET" or self == "SELLLIMIT"
 
 class order(dict):
-
+	"""Implementation of order"""
 	next_id = 0
 
 	@classmethod
 	def get_next_id(self):
+		"""Get the next order id"""
 		n = self.next_id
 		self.next_id += 1
 		return n
@@ -213,42 +231,55 @@ class order(dict):
 			return "<order:%d/%s %s at %s for %s>" %(self["id"],self["ordertype"],self["quantity"],self["price"],self["duration"])
 
 	def iscancel(self):
+		"""Test if order is cancelled"""
 		return self["ordertype"].iscancel()
 
 	def ismarket(self):
+		"""Test if market order"""
 		return self["price"] == None
 
 	def islimit(self):
+		"""Test if limit order"""
 		return not self["price"] == None
 
 	def isbuy(self):
+		"""Test if buy order"""
 		return self["ordertype"].isbuy()
 
 	def issell(self):
+		"""Test if sell order"""
 		return self["ordertype"].issell()
 
 	def get_price(self):
+		"""Get order price"""
 		return self["price"]
 
 	def set_price(self,x):
+		"""Set order price"""
 		self["price"] = x
 
 	def get_quantity(self):
+		"""Get order quantity"""
 		return self["quantity"]
 
 	def set_quantity(self,x):
+		"""Set order quantity"""
 		self["quantity"] = x
 
 	def add_quantity(self,x):
+		"""Add to order quantity"""
 		self["quantity"] += x
 
 	def add_amount(self,x):
+		"""Add to order amount"""
 		self["amount"] += x
 
 	def add_value(self,x):
+		"""Add to order value"""
 		self["value"] += x
 
 	def get_divisible(self):
+		"""Get order divisibility flag"""
 		return self["divisible"]
 
 	def __lt__(self,a):
