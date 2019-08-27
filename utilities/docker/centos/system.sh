dc7b9e7da445fce43e0f5630eba32feb1dffeb17
aivanova5 <ivanova.alyona5@gmail.com>
2019-08-27 13:29:57 -0700

    Reverted changes for gridlabd/slac-master rebuild

--- utilities/docker/centos/system.sh
+++ utilities/docker/centos/system.sh
@@ -1,3 +1,4 @@
+
 echo '
 #####################################
 # DOCKER BUILD
@@ -6,11 +7,21 @@ echo '
 '
 
 # Install needed system tools
-yum update -y
+yum update -y ; yum clean all
+yum install systemd -y ; yum clean all
 yum groupinstall "Development Tools" -y
 yum install cmake -y 
 yum install ncurses-devel -y
+yum install epel-release -y
 yum install libcurl-devel -y
 yum install which -y
-yum clean all
 
+# python3 support needed as of 4.2
+yum --disablerepo="*" --enablerepo="epel" install python36 -y
+yum install python36 python36-devel python36-pip python36-tkinter -y
+[ -f /usr/bin/python3 ] || ln -s /usr/bin/python3.6 /usr/bin/python3
+pip3 install --upgrade pip
+ln -s /usr/local/bin/pip3 /usr/bin/pip3
+pip3 install matplotlib
+pip3 install pandas
+pip3 install mysql-connector
