0f40970967dd75c236c194b0a511a0066b7e11ab
David P. Chassin <dchassin@slac.stanford.edu>
2019-09-18 10:27:30 -0700

    Fix download build info and origin.txt output

--- utilities/update_origin.sh
+++ utilities/update_origin.sh
@@ -1,7 +1,16 @@
 #/bin/bash
-ORIGIN=$(git remote -v show | head -n 1 | cut -f1)
-COMMIT=$(git log -n 1 | head -n 1 | cut -f2 -d' ')
-URL=$(git remote get-url $ORIGIN)
-echo "# $URL/commits/$COMMIT" 
-git status -s | sed -e '1,$s/^/# /'
-git diff
+if [ -d .git ]; then
+
+    # git info is available
+    ORIGIN=$(git remote -v show | head -n 1 | cut -f1)
+    COMMIT=$(git log -n 1 | head -n 1 | cut -f2 -d' ')
+    URL=$(git remote get-url $ORIGIN)
+    echo "# $URL/commits/$COMMIT" 
+    git status -s | sed -e '1,$s/^/# /'
+    git diff
+
+else
+
+    # no git info available
+    echo "No origin info available (no git repository)"
+fi
