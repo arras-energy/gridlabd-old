556989272f45583e3b662791f3c7f7ba964a4600
David P. Chassin <dchassin@slac.stanford.edu>
2019-10-10 07:44:28 -0700

    Rename python to python_extras

--- python/Makefile.mk
+++
@@ -1,8 +0,0 @@
-dist_pkgdata_DATA += python/csv_merge/csv_merge.py
-dist_pkgdata_DATA += python/example/house.glm
-dist_pkgdata_DATA += python/example/tstat_commit.py
-dist_pkgdata_DATA += python/example/tstat_init.py
-dist_pkgdata_DATA += python/jsonglm/json2glm.py
-dist_pkgdata_DATA += python/jsonglm/test_json2glm.py
-dist_pkgdata_DATA += python/volt_dump/meter_record.py
-dist_pkgdata_DATA += python/volt_dump/voltdump.py
---
+++ python_extras/Makefile.mk
@@ -0,0 +1,8 @@
+dist_pkgdata_DATA += python_extras/csv_merge/csv_merge.py
+dist_pkgdata_DATA += python_extras/example/house.glm
+dist_pkgdata_DATA += python_extras/example/tstat_commit.py
+dist_pkgdata_DATA += python_extras/example/tstat_init.py
+dist_pkgdata_DATA += python_extras/jsonglm/json2glm.py
+dist_pkgdata_DATA += python_extras/jsonglm/test_json2glm.py
+dist_pkgdata_DATA += python_extras/volt_dump/meter_record.py
+dist_pkgdata_DATA += python_extras/volt_dump/voltdump.py
