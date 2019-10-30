154e820ef2229ac3952b0bbbf8c760c1c86e0bdd
David P. Chassin <dchassin@slac.stanford.edu>
2019-10-30 13:02:08 -0700

    Add library subcommand (#356)
    * Added support for general subcommands
    
    * Added weather subcommand
    
    * Update gridlabd-weather
    
    * Update gridlabd-weather
    
    * Update gridlabd-weather
    
    * Update gridlabd-weather
    
    * Fixed problem with access to share folder
    
    * Added support for #exec command to catch non-zero exit codes
    
    * Added weather subcommand autotests
    
    * Fixed weather data install problem
    
    * Update Makefile.mk
    
    * Update Makefile.mk
    
    * Update gridlabd-weather
    
    * Update Makefile.mk
    
    * Update gridlabd-weather
    
    * Fixed docker problem with subcommand scripts
    
    * Update config.yml
    
    * Update config.yml
    
    * Update gridlabd-weather
    
    * Update config.yml
    
    * Update config.yml
    
    * Update gridlabd-weather
    
    * Update config.yml
    
    * Update config.yml
    
    * Update config.yml
    
    * Update Makefile.am
    
    * Update config.yml
    
    * Update config.yml
    
    * Update Makefile.am
    
    * Update config.yml
    
    * Update Makefile.am
    
    * Update config.yml
    
    * Update Makefile.am
    
    * Fixed weather subcommand validation error
    
    * Fixed weather subcommand autotest diff errors
    
    * Update gridlabd-weather
    
    * Update gridlabd-weather
    
    * Added library subcommand
    
    * Update gridlabd-library
    
    * Added stub of library python API
    
    * Add filesave_options global variable
    
    This option applies to both GLM and JSON files and can be used to limit which parts of the model are saved to the output file.
    
    * Add support for copy library command
    
    * Update gridlabd-library
    
    * Update gridlabd-library
    
    * Update gridlabd-library
    
    * Update gridlabd-library
    
    * Add support for automatic output conversion
    
    * Restructure of python converters
    
    * Add parent class properties in object_get
    
    * Update json2txt.py
    
    * Added support for PNG output
    
    * Update json.cpp
    
    * Update json2png.py
    
    * Remove file_converter_options
    
    * Update json2png.py
    
    * Update json2png.py
    
    * Update json2png.py
    
    * Update json2png.py
    
    * Update json2png.py
    
    * Added stub for oneline type in json2png.py
    
    * Fixed support for output PNG types
    
    * Update json2png-oneline.py
    
    * Update json2png-oneline.py
    
    * Create network_tree.ipynb
    
    * Update json2png-oneline.py
    
    * Update save.cpp
    
    * Update Makefile.mk

--- gldcore/find.h
+++ gldcore/find.h
@@ -273,7 +273,7 @@ void find_pgm_delete(FINDPGM *pgm);
 PGMCONSTFLAGS find_pgmconstants(FINDPGM *pgm);
 
 // Function: find_file
-const char *find_file(const char *name, const char *path, int mode, char *buffer, int len);
+const char *find_file(const char *name, const char *path=NULL, int mode=R_OK, char *buffer=NULL, int len=1024);
 
 // Function: find_make_invariant
 FINDPGM *find_make_invariant(FINDPGM *pgm, int mode);
--- gldcore/json.cpp
+++ gldcore/json.cpp
@@ -437,7 +437,7 @@ int GldJsonWriter::write_objects(FILE *fp)
 		if ( obj->out_svc > TS_ZERO && obj->out_svc < TS_NEVER ) TUPLE("out","%llu",(int64)(obj->out_svc));
 		TUPLE("rng_state","%llu",(int64)(obj->rng_state));
 		TUPLE("heartbeat","%llu",(int64)(obj->heartbeat));
-		(len += write(",\n\t\t\t\"%s\" : \"0x%llx%llx\"","guid",(int64)(obj->guid[0]),(int64)(obj->guid[1])));
+		(len += write(",\n\t\t\t\"%s\" : \"%llX%llX\"","guid",(int64)(obj->guid[0]),(int64)(obj->guid[1])));
 		TUPLE("flags","0x%llx",(int64)(obj->flags));
 		for ( pclass = obj->oclass ; pclass != NULL ; pclass = pclass->parent )
 		{
@@ -467,6 +467,22 @@ int GldJsonWriter::write_objects(FILE *fp)
 	                {
 	                	// no output allowed for this property
 	                }
+                }
+                else if ( prop->ptype == PT_double )
+                {
+                	double *x = object_get_double_quick(obj,prop);
+                	if ( prop->unit )
+						len += write(",\n\t\t\t\"%s\": \"%g %s\"", prop->name, *x, prop->unit->name);
+					else
+						len += write(",\n\t\t\t\"%s\": \"%g\"", prop->name, *x);
+                }
+                else if ( prop->ptype == PT_complex )
+                {
+					complex *c = object_get_complex_quick(obj,prop);
+					if ( prop->unit )
+						len += write(",\n\t\t\t\"%s\": \"%g%+gj %s\"", prop->name, c->Re(), c->Im(), prop->unit->name);
+					else
+						len += write(",\n\t\t\t\"%s\": \"%g%+gj\"", prop->name, c->Re(), c->Im());
                 }
                 else
                 {
@@ -506,12 +522,30 @@ int GldJsonWriter::write_output(FILE *fp)
 	json = fp;
 	len += write("{\t\"application\": \"gridlabd\",\n");
 	len += write("\t\"version\" : \"%u.%u.%u\"",global_version_major,global_version_minor,version);
-	len += write_modules(fp);
-	len += write_properties(fp);
-	len += write_classes(fp);
-	len += write_globals(fp);
-	len += write_objects(fp);
-	len += write_schedules(fp);
+	if ( (global_filesave_options&FSO_MODULES) == FSO_MODULES )
+	{
+		len += write_modules(fp);
+	}
+	if ( (global_filesave_options&FSO_PROPERTIES) == FSO_PROPERTIES )
+	{
+		len += write_properties(fp);
+	}
+	if ( (global_filesave_options&FSO_CLASSES) == FSO_CLASSES )
+	{
+		len += write_classes(fp);
+	}
+	if ( (global_filesave_options&FSO_GLOBALS) == FSO_GLOBALS )
+	{
+		len += write_globals(fp);
+	}
+	if ( (global_filesave_options&FSO_SCHEDULES) == FSO_SCHEDULES )
+	{
+		len += write_schedules(fp);
+	}
+	if ( (global_filesave_options&FSO_OBJECTS) == FSO_OBJECTS )
+	{
+		len += write_objects(fp);
+	}
 	len += write("\n}\n");
 	output_debug("GldJsonWriter::output() wrote %d bytes",len);
 	return len;
