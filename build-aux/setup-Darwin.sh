ae51c3e5c3215f7eae9bddb75aa3b37ff00173fc
David P. Chassin <dchassin@slac.stanford.edu>
2019-11-10 06:36:09 -0800

    Fix manual install fallback

--- build-aux/setup-Darwin.sh
+++ build-aux/setup-Darwin.sh
@@ -2,7 +2,7 @@
 RELEASE=$(uname -r | cut -f1 -d.)
 SETUP="${0/.sh/-$RELEASE.sh}"
 if [ ! -f "$SETUP" ]; then
-	${0/.sh/-manual.sh}
+	${0/Darwin.sh/manual.sh}
     echo "manual install required"
     exit 1
 fi
--- build-aux/setup-Linux.sh
+++ build-aux/setup-Linux.sh
@@ -7,7 +7,7 @@ fi
 source "/etc/os-release"
 SETUP="${0/.sh/-$ID-$(echo $VERSION_ID | cut -f1 -d.).sh}"
 if [ ! -f "$SETUP" ]; then
-	${0/.sh/-manual.sh}
+	${0/Linux.sh/manual.sh}
     echo "manual install required"
     exit 1
 fi
