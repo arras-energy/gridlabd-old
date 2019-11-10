9bc9c16e459a4e39870f7841767964c27f8cf55c
David P. Chassin <dchassin@slac.stanford.edu>
2019-11-10 06:38:22 -0800

    Fix manual install messages

--- build-aux/setup-Linux.sh
+++ build-aux/setup-Linux.sh
@@ -8,7 +8,7 @@ source "/etc/os-release"
 SETUP="${0/.sh/-$ID-$(echo $VERSION_ID | cut -f1 -d.).sh}"
 if [ ! -f "$SETUP" ]; then
 	${0/Linux.sh/manual.sh}
-    echo "manual install required"
+    echo "ERROR: $SETUP not found: manual install required"
     exit 1
 fi
 if [ "${USER:-root}" == "root" ]; then
