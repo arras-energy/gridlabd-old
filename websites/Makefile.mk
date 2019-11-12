a039d33803e6fc1c1921c27b9825ecf190c56f9d
David P. Chassin <dchassin@slac.stanford.edu>
2019-11-12 05:53:58 -0800

    Update Makefile.mk

--- websites/Makefile.mk
+++ websites/Makefile.mk
@@ -2,7 +2,7 @@ cloud-deploy: aws-deploy gcp-deploy azure-deploy
 
 aws-deploy:
 if HAVE_AWSCLI
-	@echo Uploading files to AWS websites...
+	@echo "Uploading files to AWS websites"
 	@aws s3 cp websites/docs.gridlabd.us/index.html s3://docs.gridlabd.us/index.html
 	@aws s3 cp websites/code.gridlabd.us/index.html s3://code.gridlabd.us/index.html
 	@aws s3 cp websites/www.gridlabd.us/index.html s3://www.gridlabd.us/index.html
