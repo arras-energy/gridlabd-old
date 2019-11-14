76f8583162ac2f0228df8624e8696e4d7534c93c
David P. Chassin <dchassin@slac.stanford.edu>
2019-11-14 14:33:14 -0800

    Resolved conflicts

--- docs/_defaults.js
+++
@@ -1,9 +0,0 @@
-// configuration of repo docs
-default_host = "github.com";
-default_owner = "dchassin";
-default_project = "gridlabd";
-default_branch = "master";
-default_doc = "README.md";
-
-// configuration of display
-left_panel_width = 300;
--- docs/_header.md
+++
@@ -1,3 +0,0 @@
-# [GridLAB-D 4.2](http://www.gridlabd.us/) User Documentation
-
-## ( [[Features]] ) ( [[Getting Started]] ) ( [[Documentation]] ) ( [[Developers]] ) ( [[Sponsors]] ) ( [[About Us]] ) ( [[News]] ) 
