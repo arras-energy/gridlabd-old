27284277332f6e011ab701109aefbbb0d0f1d1c2
David P. Chassin <dchassin@slac.stanford.edu>
2019-11-14 15:54:24 -0800

    Add aws subcommand (#425)
    * Add support for AWS subcommand
    * Merged scripts

--- gldcore/scripts/Makefile.mk
+++ gldcore/scripts/Makefile.mk
@@ -1,5 +1,8 @@
-bin_SCRIPTS += gldcore/scripts/gridlabd-weather
-bin_SCRIPTS += gldcore/scripts/gridlabd-manual
-bin_SCRIPTS += gldcore/scripts/gridlabd-version
-bin_SCRIPTS += gldcore/scripts/gridlabd-json-get
+bin_SCRIPTS += gldcore/scripts/gridlabd-aws
 bin_SCRIPTS += gldcore/scripts/gridlabd-git
+bin_SCRIPTS += gldcore/scripts/gridlabd-json-get
+bin_SCRIPTS += gldcore/scripts/gridlabd-library
+bin_SCRIPTS += gldcore/scripts/gridlabd-manual
+bin_SCRIPTS += gldcore/scripts/gridlabd-python
+bin_SCRIPTS += gldcore/scripts/gridlabd-version
+bin_SCRIPTS += gldcore/scripts/gridlabd-weather
