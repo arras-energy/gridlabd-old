1cb1196071b6bfb5b2365a42604e5c6460bf2f45
David P. Chassin <dchassin@slac.stanford.edu>
2019-12-02 20:33:32 -0800

    Fixed correlation autotest

---
+++ gldcore/autotest/check_correlation.py
@@ -0,0 +1,15 @@
+import sys, pandas, numpy;
+
+if len(sys.argv) > 0:
+	data = pandas.read_csv(sys.argv[1],header=None);
+	cov = numpy.cov(m=data[1],y=data[2])
+	s1 = eval(sys.argv[2]);
+	s12 = eval(sys.argv[3]);
+	s2 = eval(sys.argv[4]);
+	err = eval(sys.argv[5]);
+	maxerr = numpy.max(numpy.abs([[s1,s12],[s12,s2]]-cov))
+	if maxerr > err :
+		print(sys.argv[0],": covariance error is too large")
+		quit(1);
+else:
+    raise Exception("Syntax: python3 check_correlation.py <s1> <s2> <maximum-error>")
