d6734352155fc4fbb9b5893da924dd5d9be20653
aivanova5 <ivanova.alyona5@gmail.com>
2020-01-13 16:26:15 -0800

    Fixing the install of gridlabd develop on docker centos

---
+++ docker/gridlabd.sh
@@ -0,0 +1,22 @@
+REPO=${REPO:-https://github.com/slacgismo/gridlabd}
+BRANCH=${BRANCH:-master}
+echo "
+#####################################
+# DOCKER BUILD
+#   gridlabd <- $REPO/$BRANCH
+#####################################
+"
+
+# gridlabd source
+cd /usr/local/src
+git clone $REPO gridlabd -b $BRANCH
+if [ ! -d /usr/local/src/gridlabd ]; then
+	echo "ERROR: unable to download $REPO/$BRANCH"
+	exit 1
+fi
+
+cd gridlabd 
+bash ./install.sh --verbose
+export PATH=$(build-aux/version.sh --install)/bin/gridlabd version set
+export LD_LIBRARY_PATH=.:${LD_LIBRARY_PATH:-.}
+
