70b071b3afe963e32a9fe6ac66f1aa262c6ad4f1
David P. Chassin <dchassin@slac.stanford.edu>
2020-01-23 06:06:02 -0800

    Initial implementation of industrial load

--- industrial/industrial.cpp
+++ industrial/industrial.cpp
@@ -10,6 +10,7 @@ EXPORT_PRECOMMIT(industrial);
 CLASS *industrial::oclass = NULL;
 industrial *industrial::defaults = NULL;
 
+char256 industrial::load_property_name = "base_power_";
 char1024 industrial::naics_data_file = "naics_data_file.csv";
 naics *industrial::naics_data = NULL;
 
@@ -27,7 +28,28 @@ industrial::industrial(MODULE *module)
 		defaults = this;
 		if (gl_publish_variable(oclass,
 			
-			PT_int32,"naics_code",get_naics_code_offset(), PT_DESCRIPTION,"NAICS facility code",
+			PT_int32, "naics_code", get_naics_code_offset(), 
+				PT_DESCRIPTION, "NAICS facility code",
+
+			PT_double, "power_pf", get_power_pf_offset(), 
+				PT_DEFAULT, "1.0", 
+				PT_DESCRIPTION, "power factor for constant power load",
+
+			PT_double, "impedance_pf", get_power_pf_offset(), 
+				PT_DEFAULT, "1.0", 
+				PT_DESCRIPTION, "power factor for constant impedance load",
+
+			PT_double, "stall_voltage[pu]", get_stall_voltage_offset(),
+				PT_DEFAULT, "0.6 pu",
+				PT_DESCRIPTION, "voltage at which motors will stall",
+
+			PT_double, "start_voltage[pu]", get_start_voltage_offset(),
+				PT_DEFAULT, "0.7 pu",
+				PT_DESCRIPTION, "voltage at which motors will start",
+
+			PT_double, "total_power[MW]", get_total_power_offset(),
+				PT_DEFAULT, "0 MW",
+				PT_DESCRIPTION, "total facility power demand",
 
 			NULL)<1){
 				char msg[256];
@@ -36,6 +58,9 @@ industrial::industrial(MODULE *module)
 		}
 		gl_global_create("industrial::naics_data_file",PT_char1024,&naics_data_file,
 			PT_DESCRIPTION, "data file that contains NAICS load data model",
+			NULL);
+		gl_global_create("industrial::load_property_name",PT_char256,&load_property_name,
+			PT_DESCRIPTION, "parent base load property name to update",
 			NULL);
 	}
 }
@@ -49,14 +74,42 @@ int industrial::init(OBJECT *parent)
 {
 	naics_data = new naics(naics_data_file);
 	naics::RECORD &zip = naics_data->find(naics_code);
+
+	double power_fraction = zip.a + zip.b + zip.c + zip.d + zip.e;
+	gld_property(parent,"power_fraction_A").setp(power_fraction);
+	gld_property(parent,"power_fraction_B").setp(power_fraction);
+	gld_property(parent,"power_fraction_C").setp(power_fraction);
+
+	gld_property(parent,"current_fraction_A").setp(zip.i);
+	gld_property(parent,"current_fraction_B").setp(zip.i);
+	gld_property(parent,"current_fraction_C").setp(zip.i);
+
+	gld_property(parent,"impedance_fraction_A").setp(zip.z);
+	gld_property(parent,"impedance_fraction_B").setp(zip.z);
+	gld_property(parent,"impedance_fraction_C").setp(zip.z);
+
+	base_load[0] = new gld_property(parent,std::string(load_property_name).append("A").c_str());
+	base_load[1] = new gld_property(parent,std::string(load_property_name).append("B").c_str());
+	base_load[2] = new gld_property(parent,std::string(load_property_name).append("C").c_str());
+	if ( ! base_load[0] || ! base_load[1] || ! base_load[2] )
+		exception("unable to link base load property");
 	return 1;
 }
 
 TIMESTAMP industrial::precommit(TIMESTAMP t1)
 {
-
+	double phase_power = total_power/3;
+	base_load[0]->setp(phase_power);
+	base_load[1]->setp(phase_power);
+	base_load[2]->setp(phase_power);
 	return TS_NEVER;
 }
+
+/////////////////////////////////////////////////
+//
+// NAICS database
+//
+/////////////////////////////////////////////////
 
 naics::naics(const char *filename)
 {
@@ -66,9 +119,22 @@ naics::naics(const char *filename)
 		throw "unable to open naics data file";
 	}
 	char line[1024];
+	int linenum = 0;
 	while ( fgets(line,sizeof(line),fp) != NULL )
 	{
-		add(line);
+		linenum++;
+		try
+		{
+			add(line);
+		}
+		catch (const char *error)
+		{
+			if ( linenum > 1 )
+			{
+				gl_error("%s(%d): %s", filename, linenum, error);
+				break;
+			}
+		}
 	}
 	fclose(fp);
 }
@@ -83,10 +149,17 @@ void naics::add(const char *str)
 	RECORD *item = new RECORD;
 	if ( item == NULL )
 		throw "memory allocation failed";
-	if ( sscanf(str,"%d,%*s,%*s,%lg,%lg,%lg,%lg,%lg,%lg,%lg",
+	if ( sscanf(str,"%d,%*[^,],%*[^,],%lg,%lg,%lg,%lg,%lg,%lg,%lg",
 		&item->n, &item->a, &item->b, &item->c, &item->d, &item->e, &item->z, &item->i) == 8 )
 	{
-		data.insert(data.end(),*item);
+		if ( item->n > 0 ) // ignore "0" codes (undefined NAICS code)
+		{
+			data.insert(data.end(),*item);
+		}
+	}
+	else
+	{
+		throw "invalid NAICS record";
 	}
 }
 
--- industrial/industrial.h
+++ industrial/industrial.h
@@ -17,7 +17,7 @@ public:
 private:
 	std::list<RECORD> data;
 public:
-	naics(const char *file);
+	naics(const char *filename);
 	~naics(void);
 	void add(const char *str);
 	RECORD &find(int n);
@@ -26,12 +26,22 @@ public:
 class industrial : public gld_object {
 
 public:
+	static char256 load_property_name;
 	static char1024 naics_data_file;
 	static naics *naics_data;
 
 public:
 
 	GL_ATOMIC(int32,naics_code);
+	GL_ATOMIC(double,power_pf);
+	GL_ATOMIC(double,impedance_pf);
+	GL_ATOMIC(double,stall_voltage);
+	GL_ATOMIC(double,start_voltage);
+	GL_ATOMIC(double,total_power);
+
+private:
+
+	gld_property *base_load[3];
 
 public:
 
