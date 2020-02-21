c31eea922b1dc2fafb8844a9eddf580c0b28f147
Alyona Ivanova <Ivanova.alyona5@gmail.com>
2020-02-21 14:18:52 -0800

    Update Makefile.mk

--- gldcore/converters/Makefile.mk
+++ gldcore/converters/Makefile.mk
@@ -1,14 +1,7 @@
-#
-# json converters
-#
 dist_pkgdata_DATA += gldcore/converters/json2glm.py
 dist_pkgdata_DATA += gldcore/converters/json2txt.py
 dist_pkgdata_DATA += gldcore/converters/json2png.py
 dist_pkgdata_DATA += gldcore/converters/json2png-oneline.py
-
-#
-# csv converters
-#
 dist_pkgdata_DATA += gldcore/converters/csv2glm.py
 dist_pkgdata_DATA += gldcore/converters/csv-ceus2glm-ceus.py
 dist_pkgdata_DATA += gldcore/converters/csv-ami2glm-rbsa.py
