c2268f03da67aad6fda150e1414e6c6d4662ebb0
David P. Chassin <dchassin@slac.stanford.edu>
2020-03-08 09:08:42 -0700

    Update json.h

--- gldcore/json.h
+++ gldcore/json.h
@@ -43,7 +43,6 @@ private:
 		} str;
 	} data;
 public:
-	inline GldJson(GldJson &j) { type = j.get_type(); refs++; }; // TODO: copy constructor
 	inline GldJson(JSONTYPE t = JSON_NULL) : type(t) {};
 	inline GldJson(double x) : type(JSON_NUMBER) { data.num = x; };
 	inline GldJson(const char *x) : type(JSON_STRING) { data.str.buf = strdup(x); data.str.len = strlen(x); };
