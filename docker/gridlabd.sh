e7b3b2ef638de7a72bd004113dc2edca81b67d25
aivanova5 <ivanova.alyona5@gmail.com>
2020-03-23 11:14:10 -0700

    Update gridlabd.sh

--- docker/gridlabd.sh
+++ docker/gridlabd.sh
@@ -22,4 +22,13 @@ make -j30 install
 if [ -x "$INSTALL/bin/gridlabd-version" -a "$TEST" == "yes" ]; then
 	log "ACTIVATE: automatic"
 	run $INSTALL/bin/gridlabd version set "$VERSION"
+elif [ "$LINK" == "yes" ]; then
+	log "ACTIVATE: manual"
+	[ ! -L "$PREFIX/opt/gridlabd/current" ] && run sudo rm -f "$PREFIX/opt/gridlabd/current"
+	run sudo ln -sf "$INSTALL" "$PREFIX/opt/gridlabd/current"
+	for dir in bin lib include share; do
+		run sudo ln -sf $PREFIX/opt/gridlabd/current/$dir/gridlabd $PREFIX/$dir/gridlabd
+	done
+	run sudo ln -sf $PREFIX/opt/gridlabd/current/bin/gridlabd.bin $PREFIX/bin/gridlabd.bin
+fi
 make validate
