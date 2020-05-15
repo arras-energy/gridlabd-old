15a764089d0e86994ef24d54d0b488bb0f07af96
David P. Chassin <dchassin@slac.stanford.edu>
2020-05-15 09:29:25 -0700

    Fix python example 3

--- gldcore/link/python/examples/example_3/main.py
+++ gldcore/link/python/examples/example_3/main.py
@@ -1,4 +1,4 @@
-""" gldcore/python/link/examples/example_1/main.py
+""" gldcore/python/link/examples/example_3/main.py
 
 This is an example of a main Python module that runs a simulation and plots
 the collected data to a PNG file.  This process is completed in two parts.
@@ -15,6 +15,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import gridlabd
+import datetime as dt
 
 #
 # Part 1 - run the simulation
@@ -30,6 +31,9 @@ gridlabd.start('wait')
 # Part 2 - plot the results
 #
 
-data = pd.read_csv("x.csv",header=9,names=["datetime","x"],index_col="datetime")
-print(data.index)
+data = pd.read_csv("x.csv",names=["datetime","x"],index_col="datetime")
+index = np.array(list(map(lambda t:dt.datetime.strptime(t[0:-4],'%Y-%m-%d %H:%M:%S').timestamp(),data.index)))
+data.index = (index-index[0])/3600
+plot = data.plot(grid=True,xticks=range(0,25,5))
+plot.get_figure().savefig("example.png")
 
