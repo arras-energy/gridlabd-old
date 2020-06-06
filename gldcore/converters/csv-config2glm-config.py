c345401f25905956f5788a7edd65db084fdb68f2
David P. Chassin <dchassin@slac.stanford.edu>
2020-06-06 10:02:21 -0700

    Update csv-config2glm-config.py

--- gldcore/converters/csv-config2glm-config.py
+++ gldcore/converters/csv-config2glm-config.py
@@ -3,8 +3,12 @@
 import csv
 
 def convert(input_file, output_file, options={}):
+    if "prefix" in options.keys():
+        prefix = options["prefix"]
+    else:
+        prefix = ""
     with open(input_file,"r") as csvfile:
         reader = csv.reader(csvfile)
         with open(output_file,"w") as glmfile:
             for row in reader:
-                glmfile.write(f"#define {row[0]}=\"{row[1]}\"\n")
+                glmfile.write(f"#define {prefix}{row[0]}={row[1]}\n")
