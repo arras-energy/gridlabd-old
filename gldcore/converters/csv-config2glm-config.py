ea7fb737c2d2e37ac200a9f1ed1806526df77ce0
David P. Chassin <dchassin@slac.stanford.edu>
2020-06-06 10:13:38 -0700

    Update csv-config2glm-config.py

--- gldcore/converters/csv-config2glm-config.py
+++ gldcore/converters/csv-config2glm-config.py
@@ -1,8 +1,15 @@
-"""Convert a CSV configuration list a GLM config file
-"""
 import csv
 
 def convert(input_file, output_file, options={}):
+    """Convert a CSV configuration list a GLM config file
+
+    Parameters:
+        input_file (str) - input file name (csv file)
+        ouput_file (str) - output file name (glm file)
+        options (dict)
+            "prefix" (str) - prefix to use on GLM global names (defaut "")
+            "relax" (bool) - specify set/define relaxation (default None)
+    """
     if "prefix" in options.keys():
         prefix = options["prefix"]
     else:
@@ -10,5 +17,7 @@ def convert(input_file, output_file, options={}):
     with open(input_file,"r") as csvfile:
         reader = csv.reader(csvfile)
         with open(output_file,"w") as glmfile:
+            if "relax" in options.keys():
+                glmfile.write(f"#set strictnames={str(not options['relax']).upper()}")
             for row in reader:
                 glmfile.write(f"#define {prefix}{row[0]}={row[1]}\n")
