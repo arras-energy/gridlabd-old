18a4d0cc9fd43c36abcfbe53ebb337839401d4d0
David P. Chassin <dchassin@slac.stanford.edu>
2020-09-22 16:05:49 -0700

    Develop add optimize module (#746)
    * Add optimize module
    
    * Add optimize module
    
    * Remove CSV output from optimize module
    
    * Change optimize extremum test to optional
    
    * Rename test_extremum_opt.glm to test_extremum.glm
    
    Co-authored-by: Alyona Ivanova <Ivanova.alyona5@gmail.com>

--- optimize/Makefile.mk
+++ optimize/Makefile.mk
@@ -9,8 +9,5 @@ optimize_optimize_la_LDFLAGS += $(AM_LDFLAGS)
 optimize_optimize_la_LIBADD =
 
 optimize_optimize_la_SOURCES =
-optimize_optimize_la_SOURCES += optimize/init.cpp
-optimize_optimize_la_SOURCES += optimize/main.cpp
-optimize_optimize_la_SOURCES += optimize/optimize.h
-optimize_optimize_la_SOURCES += optimize/simple.cpp
-optimize_optimize_la_SOURCES += optimize/simple.h
+optimize_optimize_la_SOURCES += optimize/main.cpp optimize/optimize.h
+optimize_optimize_la_SOURCES += optimize/simple.cpp optimize/simple.h
