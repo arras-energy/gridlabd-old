d8c5edb4b033a0ff30e5a115b3f869d614e47a2a
David P. Chassin <dchassin@slac.stanford.edu>
2020-10-04 12:51:30 -0700

    Remove invalid data

--- industrial/data/mecs.py
+++ industrial/data/mecs.py
@@ -1,6 +1,8 @@
 from openpyxl import load_workbook
 from pandas import DataFrame, concat
 
+import config
+
 table5 = load_workbook(filename="MECS2014_Table_5-1.xlsx")["Table 5.1"]
 table9 = load_workbook(filename="MECS2014_Table_9-1.xlsx")["Table 9.1"]
 table11 = load_workbook(filename="MECS2014_Table_11-1.xlsx")["Table 11.1"]
@@ -21,8 +23,8 @@ for values in table9.iter_rows(min_row=18,min_col=1,max_row=98,max_col=7,values_
 	data.append(row)
 mecs9 = DataFrame.from_records(data,index=["NAICS"],
 			columns = ["NAICS","DESCRIPTION","FLOORAREA[Msf]","SITES[unit]","FLOORAREA_PER_SITE[sf/unit]","BUILDINGS[unit]","BUILDINGS_PER_SITE[unit/unit]"])
-
 valid_codes = mecs9.index
+print("Loaded table 9.1 ok")
 
 #
 # Table 11 - Regional electricity use
@@ -38,34 +40,29 @@ data = []
 for region, rows in regions.items():
 		for values in table11.iter_rows(min_row=rows[0],min_col=1,max_row=rows[1],max_col=7,values_only=True):
 			try:
-				index = int(values[0])
+				index = int(values[0].strip())
 				if index in valid_codes:
 					row = [index,region]
-					row.extend(values[2:])
+					for value in values[2:]:
+						if value == "W": # withheld due to low sample count
+							value = float('nan')
+						elif value == "Q": # withheld due to high RSE
+							value = float('nan')
+						elif value == "*": # value < 0.5
+							value = 0.0
+						elif value == "NA": # value not available
+							value = float('nan')
+						row.append(value)
 					data.append(row)
 			except:
 				pass
 mecs11 = DataFrame.from_records(data,index=["NAICS"],
 			columns=["NAICS","REGION","PURCHASES[GWh]","TRANSFERS[GWh]","GENERATION[GWh]","SALES[GWh]","NET_DEMAND[GWh]"])
+print("Loaded table 11.1 ok")
 
 #
 # Table 5 - End-use decomposition
 #
-enduses = {
-	"Conventional Boiler Use" : {"B":0.1,"Z":0.9}, # heaters and pumps
-	"CHP and/or Cogeneration Process" : {"B":0.5,"C":0.5}, # pumps and fans
-	"Process Heating" : {"B":0.1,"Z":0.9}, # heaters and pumps
-	"Process Cooling and Refrigeration" : {"A":0.8,"B":0.1,"C":0.1}, # compressors, pumps, and fans
-	"Machine Drive" : {"B":1.0}, # machine drives
-	"Electro-Chemical Processes" : {"A":0.4,"B":0.05,"C":0.05,"Z":0.5}, # compressors, pumps, fans, and heaters
-	"Other Process Use" : {"B":1.0},
-	"Facility HVAC (g)" : {"A":0.4,"B":0.05,"C":0.05,"Z":0.5},
-	"Facility Lighting" : {"E":0.8,"I":0.2},
-	"Other Facility Support" : {"E":1.0},
-	"Onsite Transportation" : {"B":1.0},
-	"Other Nonprocess Use" : {"D":0.1,"Z":0.8,"I":0.1},
-	"End Use Not Reported" : {"D":0.1,"Z":0.8,"I":0.1},
-}
 data = []
 index = None
 electrification = [None,None,None,None]
@@ -98,9 +95,9 @@ for values in table5.iter_rows(min_row=17,min_col=1,max_row=1819,max_col=4,value
 				total = float(values[2])
 				electrification[2] = electric
 				electrification[3] = total/electric*3.412
-				components = enduses[tag]
-			elif tag in enduses.keys():
-				components = enduses[tag]
+				components = config.enduses[tag]
+			elif tag in config.enduses.keys():
+				components = config.enduses[tag]
 			if components:
 				for enduse, value in components.items():
 					enduse_composition[enduse] += value
@@ -115,14 +112,18 @@ if index:
 mecs5 = DataFrame.from_records(data,index=["NAICS"],
 			columns=["NAICS","REPORTED_ELECTRIC[GWh]","REPORTED_ELECTRIFICATION[pu]","UNREPORTED_ELECTRIC[GWh]","UNREPORTED_ELECTRIFICATION[pu]",
 			"A","B","C","D","E","I","Z"])
-
+print("Loaded table 5.1 ok")
 
 mecs = mecs9.join(mecs11).join(mecs5)
+local = mecs["REGION"]!="NERC"
+for column in ["FLOORAREA[Msf]","BUILDINGS[unit]","SITES[unit]"]:
+	mecs.at[local,column] = float('nan')
 
 mecs.index.name = "NAICS"
 mecs.reset_index(inplace=True)
-mecs.set_index(["REGION","NAICS"],inplace=True)
+mecs.set_index(["NAICS","REGION"],inplace=True)
 
-print(mecs)
+mecs.to_csv(config.output_csv)
+print(f"Saved {config.output_csv} ok")
 
-mecs.to_csv("../mecs_data.csv")
+
