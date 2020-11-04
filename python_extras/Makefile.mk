fe5f1014ab18421b4aab349ec149aed4eabc1e3b
David P. Chassin <dchassin@slac.stanford.edu>
2020-11-04 08:58:34 -0800

    Switch to openfido CLI implementation

--- python_extras/Makefile.mk
+++ python_extras/Makefile.mk
@@ -4,8 +4,3 @@ dist_pkgdata_DATA += python_extras/example/tstat_commit.py
 dist_pkgdata_DATA += python_extras/example/tstat_init.py
 dist_pkgdata_DATA += python_extras/volt_dump/meter_record.py
 dist_pkgdata_DATA += python_extras/volt_dump/voltdump.py
-dist_pkgdata_DATA += python_extras/openfido.py
-
-$(gridlabddir)/openfido/__init__.py: $(gridlabddir)/openfido.py
-	mkdir -p $(gridlabddir)/openfido
-	mv $< $@
