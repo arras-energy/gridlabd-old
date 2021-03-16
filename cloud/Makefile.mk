3b03c98bbffe80bdd900a8a748d595a5c1d19f2d
Anna Peery <avpeery@gmail.com>
2021-03-15 20:27:52 -0700

    WIP

--- cloud/Makefile.mk
+++ cloud/Makefile.mk
@@ -1,29 +1,29 @@
 
-cloud-deploy: aws-deploy gcp-deploy azure-deploy
+# cloud-deploy: aws-deploy gcp-deploy azure-deploy
 
-WEBSITES = code$(SUFFIX).gridlabd.us docs$(SUFFIX).gridlabd.us www$(SUFFIX).gridlabd.us
+# WEBSITES = code$(SUFFIX).gridlabd.us docs$(SUFFIX).gridlabd.us www$(SUFFIX).gridlabd.us
 
-aws-deploy: $(WEBSITES)
+aws-deploy: terraform apply -var=version=$(version) -var=branch=$(branch)
 
-$(WEBSITES) :
-if HAVE_AWSCLI
-if SUFFIX
-	@echo "deploying $@ for SUFFIX='$(SUFFIX)'..."
-else
-	@echo "cannot deploy $@ for SUFFIX='$(SUFFIX)'"
-endif
-endif
+# $(WEBSITES) :
+# if HAVE_AWSCLI
 # if SUFFIX
-#  	@echo "aws s3 cp cloud/websites/$@/index.html s3://$@/index.html"
-#  	@echo "aws s3api put-object-acl --bucket $@ --key index.html --acl public-read"
+# 	@echo "deploying $@ for SUFFIX='$(SUFFIX)'..."
+# else
+# 	@echo "cannot deploy $@ for SUFFIX='$(SUFFIX)'"
+# endif
+# endif
+# # if SUFFIX
+# #  	@echo "aws s3 cp cloud/websites/$@/index.html s3://$@/index.html"
+# #  	@echo "aws s3api put-object-acl --bucket $@ --key index.html --acl public-read"
+# # endif
+
+# gcp-deploy:
+# if HAVE_GCPCLI
+# 	@echo "WARNING: gcp-deploy is not implemented yet"
 # endif
 
-gcp-deploy:
-if HAVE_GCPCLI
-	@echo "WARNING: gcp-deploy is not implemented yet"
-endif
-
-azure-deploy:
-if HAVE_AZCLI
-	@echo "WARNING: azure-deploy is not implemented yet"
-endif
+# azure-deploy:
+# if HAVE_AZCLI
+# 	@echo "WARNING: azure-deploy is not implemented yet"
+# endif
