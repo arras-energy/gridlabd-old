fa7f4444dec7eea00bc1b9251f271cf37d965c0b
David P. Chassin <dchassin@slac.stanford.edu>
2021-03-29 15:11:20 -0700

    Fixed check error

--- python_extras/validate_check_model.py
+++ python_extras/validate_check_model.py
@@ -1,7 +1,10 @@
 import sys
+import gridlabd
 def on_term(t) : 
 	with open('gridlabd.err') as gld_err : 
-		assert("WARNING [check]" not in gld_err.read()  )	
+		if "ERROR [check]" not in gld_err.read():
+			gridlabd.error("check error detected")
 
 	with open('gridlabd.wrn') as gld_wrn : 
-		assert("WARNING [check]" not in gld_wrn.read()  )	
+		if "WARNING [check]" not in gld_wrn.read():
+			gridlabd.error("check warning detected")
