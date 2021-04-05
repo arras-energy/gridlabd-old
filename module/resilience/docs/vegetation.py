690e8d1aedd1e9496474b7c506a4f54e01df2f3e
David P. Chassin <dchassin@slac.stanford.edu>
2021-04-05 10:03:47 -0700

    Convert example to command-like usage

--- module/resilience/docs/vegetation.py
+++ module/resilience/docs/vegetation.py
@@ -7,19 +7,36 @@ import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
 
+def help(short=False):
+	print("Syntax: vegetation -i|--input PATHFILE -m|--mapfile MAPFILE -o|--output DATAFILE [-s|--show] [-r|--resolution METERS] [-x|--extract POS SIZE]")
+	if not short:
+		print("""
+  -i|--input PATHFILE         input CSV file name (latitude,longitude,...)
+  -m|--mapfile MAPFILE        map image file name (e.g., name.tif)
+  -o|--output DATAFILE        output CSV file name (distance,data,...)
+  -s|--show                   enable display images and plots
+  -r|--resolution METERS      image resolution in meters (default is 10)
+  -x|--extract LAT,LON SIZE   image extraction region (SIZE is in meters)
+""")
+	return 
+
 if len(sys.argv) < 2:
-	print("Syntax: vegetation -i|--input PATHFILE -m|--mapfile MAPFILE -o|--output DATAFILE [-s|--show] [-r|--resolution METERS]")
-	exit(0)
+	help(True)
+	exit(1)
 
 PATHFILE = None
 MAPFILE = None
 DATAFILE = None
 SHOWPLOT = False
 res = 10 # TIFF resolution in meters
+EXTRACT = None
 
 n = 1
 while n < len(sys.argv):
-	if sys.argv[n] in ["-i","--input"]:
+	if sys.argv[n] in ["-h","--help","help"]:
+		help()
+		exit(0)
+	elif sys.argv[n] in ["-i","--input"]:
 		n += 1
 		PATHFILE = sys.argv[n]
 	elif sys.argv[n] in ["-m","--mapfile"]:
@@ -33,41 +50,49 @@ while n < len(sys.argv):
 	elif sys.argv[n] in ["-r","--resolution"]:
  		n += 1
  		res = int(sys.argv[n]) 
+	elif sys.argv[n] in ["-x","--extract"]:
+ 		n += 1
+ 		pos = list(map(lambda x: float(x),sys.argv[n].split(",")))
+ 		EXTRACT = {"pos":{"lat":pos[0],"lon":pos[1]},"size":float(sys.argv[n+1])}
+ 		n += 1 
 	else:
 		print(f"ERROR [vegetation]: '{sys.argv[n]}' is an invalid option")
 		exit(1)
 	n += 1
 
-if not PATHFILE or not MAPFILE or not DATAFILE:
+if not ( PATHFILE or EXTRACT ) or not MAPFILE or not ( DATAFILE or EXTRACT ):
 	print(f"ERROR [vegetation]: missing required argument")
 
-def vegetation_data(PATHFILE,MAPFILE,DATAFILE,SHOWPLOT,res):
-	image_data = {
-		"MarinCounty" : {
-			"p0" : {"lat":38.131435, "lon":-122.740464},
-			"p1" : {"lat":38.110846, "lon":-122.724984},
-		},
-		"SanMateoCounty" : {
-			"p0" : {"lat":"37:47:29", "lon":"-122:31:31"},
-			"p1" : {"lat":"37:06:24", "lon":"-122:06:57"},	
-		},
+# map coordinate keys for each country (corner of images)
+# TODO: this need to be improved to accurately map pixels
+image_data = {
+	"MarinCounty" : {
+		"p0" : {"lat":38.131435, "lon":-122.740464},
+		"p1" : {"lat":38.110846, "lon":-122.724984},
+	},
+	"SanMateoCounty" : {
+		"p0" : {"lat":"37:47:29", "lon":"-122:31:31"},
+		"p1" : {"lat":"37:06:24", "lon":"-122:06:57"},	
+	},
+}
+
+# convert dms coordinates to degrees
+for county, specs in image_data.items():
+	for spec, coords in specs.items():
+		for name, coord in coords.items():
+			if type(coord) is str:
+				dms = list(map(lambda n: int(n),coord.split(":")))
+				image_data[county][spec][name] = np.sign(dms[0])*(abs(dms[0]) + (dms[1] + dms[2]/60)/60)
+p0 = image_data[county]["p0"]
+p1 = image_data[county]["p1"]
+box = {
+	"N":max(p0["lat"],p1["lat"]), 
+	"S":min(p0["lat"],p1["lat"]), 
+	"E":max(p0["lon"],p1["lon"]), 
+	"W":min(p0["lon"],p1["lon"]),
 	}
 
-	# convert dms coordinates to degrees
-	for county, specs in image_data.items():
-		for spec, coords in specs.items():
-			for name, coord in coords.items():
-				if type(coord) is str:
-					dms = list(map(lambda n: int(n),coord.split(":")))
-					image_data[county][spec][name] = np.sign(dms[0])*(abs(dms[0]) + (dms[1] + dms[2]/60)/60)
-	p0 = image_data[county]["p0"]
-	p1 = image_data[county]["p1"]
-	box = {
-		"N":max(p0["lat"],p1["lat"]), 
-		"S":min(p0["lat"],p1["lat"]), 
-		"E":max(p0["lon"],p1["lon"]), 
-		"W":min(p0["lon"],p1["lon"]),
-		}
+def vegetation_data(PATHFILE,MAPFILE,DATAFILE,SHOWPLOT,res):
 
 	def get_values(specs,path):
 
@@ -153,5 +178,21 @@ def vegetation_data(PATHFILE,MAPFILE,DATAFILE,SHOWPLOT,res):
 		plt.title(f"{MAPFILE} from {start} to {stop}")
 		plt.show()
 
+def show_area(MAPFILE,res,pos,size):
+	img = Image.open(MAPFILE)
+	if SHOWPLOT:
+		img.show()
+	data = np.array(img)
+	x0 = int((pos["lon"]-box["W"])/(box["E"]-box["W"])*data.shape[0]) - int(size/res)
+	y0 = int((pos["lat"]-box["N"])/(box["S"]-box["N"])*data.shape[1]) - int(size/res)
+	x1 = int((pos["lon"]-box["W"])/(box["E"]-box["W"])*data.shape[0]) + int(size/res)
+	y1 = int((pos["lat"]-box["N"])/(box["S"]-box["N"])*data.shape[1]) + int(size/res)
+	print(f"Original size = {data.shape}, cut region = ({x0},{y0}) - ({x1},{y1})")
+	cut = Image.fromarray(data[x0:x1,y0:y1])
+	cut.show()
+
 if __name__ == '__main__':
-	vegetation_data(PATHFILE,MAPFILE,DATAFILE,SHOWPLOT,res)
+	if EXTRACT:
+		show_area(MAPFILE,res,EXTRACT["pos"],EXTRACT["size"])
+	else:
+		vegetation_data(PATHFILE,MAPFILE,DATAFILE,SHOWPLOT,res)
