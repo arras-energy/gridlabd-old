ef2600830ba5a54c0049c0d112e8f94222a28f09
David P. Chassin <dchassin@slac.stanford.edu>
2021-04-06 12:22:16 -0700

    Update agl.py

--- module/resilience/docs/agl.py
+++ module/resilience/docs/agl.py
@@ -1,15 +1,19 @@
 import pandas as pd
+import math
 
 line_data = pd.read_csv("230kV_example.csv")
 
-left = line_data["latitude"].min()
-right = line_data["latitude"].max()
-top = line_data["longitude"].max() 
-bottom = line_data["longitude"].min()
+left = math.floor(line_data["latitude"].min())
+right = math.ceil(line_data["latitude"].max())
+top = math.ceil(line_data["longitude"].max())
+bottom = math.floor(line_data["longitude"].min())
 print(left,bottom,right,top)
 
 import elevation
 
-elevation.clip(bounds=(left,bottom,right,top),output="230kV_elevation.tif")
+elevation.clip(bounds=(left,bottom,right,top),output="elevation.tif")
 elevation.clean()
 
+from PIL import Image
+img = Image.open("elevation.tif")
+img.show()
