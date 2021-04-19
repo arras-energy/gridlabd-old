29662817ba294f2e9961c3fc11bf2bfc27f9e58e
David P. Chassin <dchassin@slac.stanford.edu>
2021-04-19 09:13:56 -0700

    Update Makefile.mk

--- gldcore/geodata/Makefile.mk
+++ gldcore/geodata/Makefile.mk
@@ -1,10 +1,9 @@
-dist_pkgdata_DATA += gldcore/geodata/geodata_distance.py
-dist_pkgdata_DATA += gldcore/geodata/geodata_distance.cfg
-dist_pkgdata_DATA += gldcore/geodata/geodata_elevation.py
-dist_pkgdata_DATA += gldcore/geodata/geodata_elevation.cfg
-dist_pkgdata_DATA += gldcore/geodata/geodata_powerline.py
-dist_pkgdata_DATA += gldcore/geodata/geodata_powerline.cfg
 dist_pkgdata_DATA += gldcore/geodata/geodata_address.py
-dist_pkgdata_DATA += gldcore/geodata/geodata_address.cfg
+dist_pkgdata_DATA += gldcore/geodata/geodata_census.py
+dist_pkgdata_DATA += gldcore/geodata/geodata_distance.py
+dist_pkgdata_DATA += gldcore/geodata/geodata_elevation.py
+dist_pkgdata_DATA += gldcore/geodata/geodata_powerline.py
+# dist_pkgdata_DATA += gldcore/geodata/geodata_transportation.py
 dist_pkgdata_DATA += gldcore/geodata/geodata_utility.py
-dist_pkgdata_DATA += gldcore/geodata/geodata_utility.cfg
+# dist_pkgdata_DATA += gldcore/geodata/geodata_vegetation.py
+# dist_pkgdata_DATA += gldcore/geodata/geodata_weather.py
