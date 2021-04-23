df8d098a7c18656a7604686be9e11acdb6a15d4a
David P. Chassin <dchassin@slac.stanford.edu>
2021-04-23 12:28:57 -0700

    Update unittest.sh

--- gldcore/geodata/autotest/unittest.sh
+++ gldcore/geodata/autotest/unittest.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -e
+set -u
+set -x
 
 DIR=${0/unittest.sh/..}
 for F in $DIR/*.py; do
-	echo "$(basename $F) unittest:" > /tmp/$$
+	echo '*** '"$(basename $F) unittest"' ***' > /tmp/$$
 	/usr/local/bin/python3 $F -q 2>>/tmp/$$ || cat /tmp/$$ >>/tmp/$$.err
 done
 rm -f /tmp/$$
-if [ -f /tmp/$$.err ]; then
+if [ -f /tmp/$$.err -a ! -s /tmp/$$.err ]; then
 	cat /tmp/$$.err >/dev/stderr
 	rm -f /tmp/$$.err
 	exit 1
