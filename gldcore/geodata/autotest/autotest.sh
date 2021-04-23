97771c1c3d17de3d0a2d9e553a498979a32cd7ae
David P. Chassin <dchassin@slac.stanford.edu>
2021-04-22 21:12:13 -0700

    Rename unittest.sh

--- gldcore/geodata/autotest/autotest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=${0/autotest.sh/..}
-for F in $DIR/*.py; do
-	echo "$(basename $F) unittest:" > /tmp/$$
-	/usr/local/bin/python3 $F -q 2>>/tmp/$$ || cat /tmp/$$ >/dev/stderr
-done
-rm -f /tmp/$$
