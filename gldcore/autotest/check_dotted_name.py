925495a3efd1a4534783e0fc244c95e1c9bf5058
David P. Chassin <dchassin@slac.stanford.edu>
2021-04-27 13:33:40 -0700

    Update check_dotted_name.py

--- gldcore/autotest/check_dotted_name.py
+++ gldcore/autotest/check_dotted_name.py
@@ -1,4 +1,4 @@
 import sys
 with open('out.csv') as f:
-	if '2021-02-01 11:50:00 PST,+315' not in f.read():
+	if '2021-02-01 11:50:00 PST,+500' not in f.read():
 		raise TypeError("Modify did not execute")
