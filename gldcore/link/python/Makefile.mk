f4795e3f1e970a8f1e0b0d8012717693fd1f7bf2
David P. Chassin <dchassin@slac.stanford.edu>
2021-04-27 08:55:23 -0700

    Update Makefile.mk

--- gldcore/link/python/Makefile.mk
+++ gldcore/link/python/Makefile.mk
@@ -1,8 +1,12 @@
-python-install:
+python-install: python-build
 	@echo "python3 $(top_srcdir)/gldcore/link/python/setup.py --quiet install"
-	@( export SRCDIR=$(top_srcdir) ; python3 $(top_srcdir)/gldcore/link/python/setup.py --quiet install )
+	@( export SRCDIR=$(top_srcdir) ; python3 $(top_srcdir)/gldcore/link/python/setup.py --quiet install)
+
+python-build:
+	@echo "python3 $(top_srcdir)/gldcore/link/python/setup.py --quiet build ${PYTHONSETUPFLAGS:--j 5}"
+	@( export SRCDIR=$(top_srcdir) ; python3 $(top_srcdir)/gldcore/link/python/setup.py --quiet build ${PYTHONSETUPFLAGS:--j 5})
 
 python-clean:
 	@echo "python3 $(top_srcdir)/gldcore/link/python/setup.py clean"
 	@( export SRCDIR=$(top_srcdir) ; python3 $(top_srcdir)/gldcore/link/python/setup.py clean )
-	-rm -rf $(SRCDIR)/build/lib.*
+	-rm -rf $(top_srcdir)/build/lib.*
