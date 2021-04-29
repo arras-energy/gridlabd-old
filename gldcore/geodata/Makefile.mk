7bd901f3cddec298a5c9c9d25af33aca62c41a86
David P. Chassin <dchassin@slac.stanford.edu>
2021-04-29 15:39:21 -0700

    Add cabletypes for powerline geodata package

--- gldcore/geodata/Makefile.mk
+++ gldcore/geodata/Makefile.mk
@@ -3,6 +3,7 @@ dist_pkgdata_DATA += gldcore/geodata/geodata_census.py
 dist_pkgdata_DATA += gldcore/geodata/geodata_distance.py
 dist_pkgdata_DATA += gldcore/geodata/geodata_elevation.py
 dist_pkgdata_DATA += gldcore/geodata/geodata_powerline.py
+dist_pkgdata_DATA += gldcore/geodata/geodata_powerline_cabletypes.csv
 # dist_pkgdata_DATA += gldcore/geodata/geodata_transportation.py
 dist_pkgdata_DATA += gldcore/geodata/geodata_utility.py
 # dist_pkgdata_DATA += gldcore/geodata/geodata_vegetation.py
