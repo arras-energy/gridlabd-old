f89d3b018bf04c0fdef1d16561ceafb858667e36
David P. Chassin <dchassin@slac.stanford.edu>
2021-05-15 19:15:23 -0700

    Tutorial update

--- gldcore/geodata/Makefile.mk
+++ gldcore/geodata/Makefile.mk
@@ -14,4 +14,4 @@ python_requirements += gldcore/geodata/requirements.txt
 docs_targets += docs/Tutorial/Geodata.md
 
 docs/Tutorial/Geodata.md: gldcore/geodata/docs/Geodata.ipynb
-	$pandoc $< -s -t gfm | sed -n '20,$$p' > $@
+	@pandoc $< -s -t gfm | sed -n '20,$$p' > $@
