9db7f68ad424eb36db146b5ea4a15a6f4edca5dc
David P. Chassin <dchassin@slac.stanford.edu>
2021-08-02 13:35:08 -0700

    Update weather.py

--- python_extras/gridlabd_app/weather.py
+++ python_extras/gridlabd_app/weather.py
@@ -8,17 +8,7 @@ from tkinter import *
 from tkinter import Menu, messagebox, filedialog, simpledialog, ttk
 import subprocess
 
-def command(text):
-    subcommand = ["gridlabd","weather"]
-    subcommand.extend(text)
-    result = subprocess.run(subcommand,capture_output=True)
-    if result.returncode == 0:
-        return result.stdout.decode('utf-8').split('\n')
-    else:
-        messagebox.showerror(f"Weather error",result.stderr)
-        return []
-
-dialog_width = 420
+dialog_width = 600
 dialog_height = 400
 
 class Weather(Tk):
@@ -28,6 +18,7 @@ class Weather(Tk):
 
         self.title("GridLAB-D Weather")
         self.configure()
+        self.focus()
 
         self.style = ttk.Style(self)
         self.style.configure('gridlabd.Treeview',
@@ -41,23 +32,42 @@ class Weather(Tk):
         self.geometry(f'{dialog_width}x{dialog_height}')
 
         self.indexview = IndexView(self)
+        self.indexview.column('#0',width=290)
         self.indexview.pack(padx=5, pady=10, side=LEFT, fill=Y)
 
         self.listview = ListView(self)
+        self.listview.column('#0',width=290)
         self.listview.pack(padx=5, pady=10, side=LEFT, fill=Y)
 
+        self.message = Label(self,text="Loading data...")
+        self.message.pack(padx=5,pady=5,side=BOTTOM, fill=X)
+
+    def command(self,text):
+        subcommand = ["gridlabd","weather"]
+        subcommand.extend(text)
+        self.config(cursor="wait")
+        self.update()
+        result = subprocess.run(subcommand,capture_output=True)
+        self.config(cursor="")
+        self.update()
+        if result.returncode == 0:
+            return result.stdout.decode('utf-8').split('\n')
+        else:
+            messagebox.showerror(f"Weather error",result.stderr)
+            return []
 
 class IndexView(ttk.Treeview):
 
     def __init__(self,main):
         ttk.Treeview.__init__(self)
         self.main = main
-        self.bind("<Double-1>",self.on_select)
+        self.bind("<Double-1>",self.download_file)
+        self.bind("<Button-2>",self.show_popup)
         self.index = {}
 
         self.heading('#0',text="Remote files")
         usa = self.insert('',END,text="US")
-        files = command(["index"])
+        files = self.main.command(["index"])
         states = []
         for file in files:
             state = file[0:2]
@@ -71,26 +81,101 @@ class IndexView(ttk.Treeview):
             tag = self.insert(items[state],END,text=file[3:].replace("_"," ").split(".")[0])
             self.index[tag] = file
 
-    def on_select(self,event):
+    def show_popup(self,event):
+        iid = self.identify_row(event.y)
+        if iid:
+            self.selection_set(iid)
+            popup = Menu(self,tearoff=0);
+            popup.add_command(label="Download",command=self.download_file)
+            popup.add_command(label="Remote...",command=self.edit_remote)
+            try:
+                popup.tk_popup(event.x_root,event.y_root,0)
+            finally:
+                popup.grab_release()
+
+    def download_file(self,event=None):
         tag = self.selection()[0]
-        file = self.index[tag]
-        if file:
-            command(["get",file])
+        if tag in self.index.keys():
+            file = self.index[tag]
+            self.main.command(["get",file])
             self.main.update()
-        else:
-            print(self.selection)
+            self.main.listview.reload()
+
+    def edit_remote(self,event=None):
+        msg = Tk()
+        msg.title("Remote settings")
+        info = self.main.command(["config","show"])
+        table = ttk.Treeview(msg,columns=['value'],show='tree')
+        for item in info:
+            spec = item.split("=")
+            if len(spec) > 1:
+                table.insert('',END,text=spec[0],values=[spec[1].replace('"','')])
+        table.column('#0',width=100)
+        table.column('value',width=500,stretch=YES)
+        table.grid(row=0, column=0)
 
 class ListView(ttk.Treeview):
 
     def __init__(self,main):
         ttk.Treeview.__init__(self)
         self.main = main
+        self.reload()
+        self.bind("<Double-1>",self.show_info)
+        self.bind("<Button-2>",self.show_popup)
 
+    def show_popup(self,event):
+        iid = self.identify_row(event.y)
+        if iid:
+            self.selection_set(iid)
+            popup = Menu(self,tearoff=0);
+            popup.add_command(label="Show info",command=self.show_info)
+            popup.add_command(label="Copy name",command=self.copy_name)
+            popup.add_separator()
+            popup.add_command(label="Delete",command=self.delete_item)
+            try:
+                popup.tk_popup(event.x_root,event.y_root,0)
+            finally:
+                popup.grab_release()
+
+    def copy_name(self):
+        tag = self.selection()[0]
+        file = self.item(tag,'text')
+        if file:
+            self.clipboard_clear()
+            self.clipboard_append(file)
+            self.update()
+
+    def delete_item(self):
+        tag = self.selection()[0]
+        file = self.item(tag,'text')
+        if file:
+            self.main.command(["delete",file])
+            self.reload()
+
+    def reload(self):
+        for item in self.get_children():
+            self.delete(item)
         self.heading('#0',text="Local files")
-        for item in command(["list"]):
+        for item in self.main.command(["list"]):
             self.insert('',END,text=item)
 
+    def show_info(self,event=None):
+        tag = self.selection()[0]
+        file = self.item(tag,'text')
+        if file:
+            info = self.main.command(["info",file])
+            names = info[0].split(",")
+            values = info[1].split(",")
+            msg = Tk()
+            msg.title(file)
+            table = ttk.Treeview(msg,columns=['value'],show='tree')
+            for spec in list(zip(names,values)):
+                table.insert('',END,text=spec[0],values=[spec[1].replace('"','')])
+            table.column('#0',width=100)
+            table.column('value',width=500)
+            table.grid(row=0, column=0)
 
 if __name__ == "__main__":
     root = Weather()
     root.mainloop()
+    quit(0)
