93e3ebd6329900b7446e57b77e76b5ef850a8521
David P. Chassin <david.chassin@me.com>
2021-08-06 07:39:38 -0700

    Add basic clipboard class

--- python_extras/gridlabd_app/editor.py
+++ python_extras/gridlabd_app/editor.py
@@ -4,7 +4,7 @@ See https://www.gridlabd.us/ for license, acknowledgments, credits, manuals, doc
 """
 
 #
-# Required modules
+# Python modules
 #
 import sys, os
 import timeit
@@ -119,6 +119,40 @@ if sys.platform == "darwin":
             info['CFBundleShortVersionString'] = f"{version}"
             info['CFBundleVersion'] = f"{build} {branch}"
             info['NSHumanReadableCopyright'] = gridlabd.copyright().split("\n\n")[1]
+
+#
+# Clipboard
+#
+
+class Clipboard:
+
+    source_value = None
+    source_type = None
+
+    def __init__(self,main):
+        self.main = main
+
+    def copy(self,type,value,append=False):
+        source_type = type
+        source_value = value
+        if not append:
+            self.main.clipboard_clear()
+        self.main.clipboard_append(json.dumps(value))
+
+    def paste(self,type='*'):
+        if type == '*' or type == source_type:
+            return source_value
+        else:
+            return None
+
+    def get_type(self):
+        return source_type
+
+    def get_value(self):
+        return source_value
+
+    def is_type(self,type):
+        return type == source_type
 
 #
 # Menu
