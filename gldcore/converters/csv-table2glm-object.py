5c58f41d902d5c4e30d489b517b750ec4a31d44b
Alyona Teyber <Ivanova.alyona5@gmail.com>
2021-08-20 14:10:43 -0700

    Fixing the table2glm converter and adding a more robust testing input

--- gldcore/converters/csv-table2glm-object.py
+++ gldcore/converters/csv-table2glm-object.py
@@ -1,7 +1,5 @@
 """Convert generable CSV table into a list of GLM objects
-
 Valid options:
-
   class:   specifies the default class to use when class is not specified in the table
 """
 import csv
@@ -18,17 +16,16 @@ def warning(msg):
 def convert (p_configuration_in, p_configuration_out, options={} ) : 
 	with open(p_configuration_in, "r", newline='') as csvfile:
 		configurations = csv.reader(csvfile, delimiter=',')
-		p_config_out = open(p_configuration_out, "a")
-		p_config_out.truncate(0)
-		p_config_out.write("// Objects \n")
-		module_list = []		
+		p_config_out = open(p_configuration_out, "w")
+		p_config_out.write(f"// {__file__} {p_configuration_in} {p_configuration_out} {options} \n")
 		for i, row in enumerate(configurations):
 			if i == 0 : 
 				headers = row
 				if "class" in headers:
-					if classname:
-						warning(f"class data overrides option 'class={classname}'")
 					class_index = headers.index("class")
+					if options:
+						warning(f"class data overrides option from file")
+					
 				else:
 					class_index = None
 			else : 
@@ -37,27 +34,21 @@ def convert (p_configuration_in, p_configuration_out, options={} ) :
 				else : 
 					class_index=headers.index("class")
 				if not row[class_index] : 
+					p_config_out.write(f"object {options['class']} ")
 					class_name = options['class']
 				if row[class_index] : 
+					p_config_out.write(f"object {row[class_index]} ")
 					class_name = row[class_index]
-				if "." in class_name :
-					class_spec = class_name.split(".")[0]
-					if class_spec not in module_list: 
-						p_config_out.write(f"module {class_spec};\n")
-						module_list.append(class_spec)
-				p_config_out.write(f"object {class_name} ")
 				p_config_out.write("{ \n")
 				for j,value in enumerate (row) : 
 					if j!=class_index and headers[j] : 
 						if not value and headers[j].strip()=="name" : 
-							p_config_out.write(f"\t{headers[j].strip()} {oclass}:{i};\n")
+							p_config_out.write("\t" + headers[j].strip() + " " + class_name +"_" + str(i) + ";\n")
 						else : 
 							if value : 
 								if re.findall('^\d+',value) or value.startswith('(') or '([0-9]*\ [*a-zA-Z+]*){0,1}?' in value and ',' not in value: 
 									p_config_out.write("\t" + headers[j].strip() + " " + value + ";\n")
 								else : 
 									p_config_out.write("\t" + headers[j].strip() + " " + "\"" +value + "\"" + ";\n")
-				for prop,value in options.items():
-					p_config_out.write(f"\t{prop} \"{value}\";\n") 
 				p_config_out.write("}\n")
 		p_config_out.close()
