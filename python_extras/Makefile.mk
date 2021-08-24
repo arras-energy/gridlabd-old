e1f94c6e0549cff6526ca38349c26ad7501de0d8
David P. Chassin <dchassin@slac.stanford.edu>
2021-08-24 15:11:02 -0700

    Change no-args to look for gridlabd-editor (#977)
    * Change no-args to look for gridlabd-editor
    
    * Add no args popup
    
    * Update cmdarg.cpp
    
    * Update gridlabd-editor.py
    
    * Update gridlabd-editor.py
    
    * Update gridlabd-editor.py
    
    Co-authored-by: Alyona Teyber <Ivanova.alyona5@gmail.com>

--- python_extras/Makefile.mk
+++ python_extras/Makefile.mk
@@ -5,3 +5,5 @@ dist_pkgdata_DATA += python_extras/example/tstat_init.py
 dist_pkgdata_DATA += python_extras/volt_dump/meter_record.py
 dist_pkgdata_DATA += python_extras/volt_dump/voltdump.py
 dist_pkgdata_DATA += python_extras/metar2glm.py
+dist_pkgdata_DATA += python_extras/gridlabd-editor.py
+dist_pkgdata_DATA += python_extras/gridlabd-editor.png
