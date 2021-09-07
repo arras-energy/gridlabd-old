e332aa63594b47174b9fcddda09d454cacc1b423
David P. Chassin <dchassin@slac.stanford.edu>
2021-09-07 10:47:00 -0700

    Update work

--- tutorials/Theory/average_1.py
+++ tutorials/Theory/average_1.py
@@ -1,11 +1,11 @@
-
-object_list = []
-y = 0.0
-n = 0
-
 def on_init(t):
+	global object_list
 	object_list = gridlabd.get("objects")
 	gridlabd.output(f"{len(object_list)} objects found")
+	global y
+	global n
+	y = 0.0
+	n = 0
 	return True
 
 def sync(obj,t):
