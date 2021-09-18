4f647ccc4cf69bddd5deecb9f91898e3fede78a7
David P. Chassin <david.chassin@me.com>
2021-09-18 09:06:34 -0700

    Update Makefile.mk

--- gldcore/Makefile.mk
+++ gldcore/Makefile.mk
@@ -137,7 +137,7 @@ gldcore/build.h: buildnum
 
 buildnum: utilities/build_number
 	@/bin/bash -c "source $(top_srcdir)/utilities/build_number $(top_srcdir) gldcore/build.h"
-	@/bin/bash -c "source utilities/update_origin.sh" > origin.txt
+	@/bin/bash -c "source $(top_srcdir)/utilities/update_origin.sh" > origin.txt
 
 weather:
 	@(echo "Installing weather data manager" && mkdir -p $(prefix)/share/gridlabd/weather && chmod 2777 $(prefix)/share/gridlabd/weather && chmod 1755 $(bindir)/gridlabd-weather)
