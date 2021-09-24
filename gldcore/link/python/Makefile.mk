c224db3b95a1d857eac79e3d0068fb185fd406d1
David P. Chassin <dchassin@slac.stanford.edu>
2021-09-24 15:44:31 -0700

    Develop fix version check (#1006)
    * Update gridlabd-version
    
    * Update gridlabd-version
    
    * Update gridlabd-version
    
    * Update gridlabd-version
    
    * Update Makefile.am
    
    * Update gridlabd.m4sh
    
    * Update gridlabd.in
    
    * Update Makefile.mk
    
    * Update gridlabd.in
    
    * Update gridlabd-version
    
    * Add support for logging version checks

--- gldcore/link/python/Makefile.mk
+++ gldcore/link/python/Makefile.mk
@@ -1,4 +1,4 @@
-python-install:
+python-install: gldcore/build.h
 	@echo "python3 $(top_srcdir)/gldcore/link/python/setup.py --quiet install"
 	@( export SRCDIR=$(top_srcdir) ; python3 $(top_srcdir)/gldcore/link/python/setup.py --quiet install )
 
