16fcacbd1f088fb10a26366542f08aa2c640d646
David P. Chassin <dchassin@slac.stanford.edu>
2021-10-12 13:49:46 -0700

    Update test_IEEE123_link_outages.py

--- module/resilience/autotest/test_IEEE123_link_outages.py
+++ module/resilience/autotest/test_IEEE123_link_outages.py
@@ -2,11 +2,10 @@
 
 import sys, datetime
 
-linklist = None
+linklist = []
 def on_init(t):
 	global linklist
 	objlist = gridlabd.get("objects")
-	linklist = []
 	for obj in objlist:
 		data = gridlabd.get_object(obj)
 		if "from" in data.keys() and "to" in data.keys():
@@ -20,12 +19,12 @@ def on_precommit(t0):
 	global t_next
 	t_next = (int(t0/3600)+1)*3600
 	global current_object
-	if current_object == None and len(linklist) > 0:
+	if not current_object and len(linklist) > 0:
 		current_object = linklist[0]
 		del linklist[0]
 		gridlabd.set_value(current_object,"status","OPEN")
 		gridlabd.output(f"on_precommit(t='{datetime.datetime.fromtimestamp(t0)}'): opening link {current_object} --> {datetime.datetime.fromtimestamp(t_next)}")
-	elif current_object != None:
+	elif current_object:
 		gridlabd.set_value(current_object,"status","CLOSED")
 		gridlabd.output(f"on_precommit(t={datetime.datetime.fromtimestamp(t0)}): closing link {current_object} --> {datetime.datetime.fromtimestamp(t_next)}")
 		current_object = None
