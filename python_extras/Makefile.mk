5492e866f5c57751c83da0e99f7e5737eaac398e
David P. Chassin <dchassin@slac.stanford.edu>
2021-10-27 10:37:21 -0700

    Add weather forecast generator (#1022)
    * Basic forecasting tool implemented
    
    * Add wind direction and units to forecast object
    
    * Update noaa_forecast.py
    
    * Update noaa_forecast.py
    
    * Update noaa_forecast.py
    
    * Add option to control interpolation
    
    * Update noaa_forecast.py
    
    * Update call method for noaa_forecast
    
    * Update noaa_forecast.py
    
    * Update noaa_forecast.py
    
    * Update noaa_forecast.py
    
    * Fix error handling
    
    * Update gridlabd.in
    
    * Update gridlabd.m4sh
    
    * Update Makefile.mk
    
    Co-authored-by: Alyona Teyber <Ivanova.alyona5@gmail.com>

--- python_extras/Makefile.mk
+++ python_extras/Makefile.mk
@@ -8,5 +8,6 @@ dist_pkgdata_DATA += python_extras/gridlabd-editor.py
 dist_pkgdata_DATA += python_extras/metar2glm.py
 dist_pkgdata_DATA += python_extras/nsrdb_weather.py
 dist_pkgdata_DATA += python_extras/ucar_weather.py
+dist_pkgdata_DATA += python_extras/noaa_forecast.py
 dist_pkgdata_DATA += python_extras/volt_dump/meter_record.py
 dist_pkgdata_DATA += python_extras/volt_dump/voltdump.py
