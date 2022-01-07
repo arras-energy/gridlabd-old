79ef8574b49b874a4a3274598061b3ffdd396cdd
JimmyLeu76 <jimmyleu@slac.stanford.edu>
2022-01-07 14:28:08 -0800

    update installation dependencies

--- build-aux/setup-Linux-debian-10.sh
+++ build-aux/setup-Linux-debian-10.sh
@@ -4,9 +4,12 @@
 # update first
 apt-get -q update
 apt-get -q install tzdata -y
-RUN apt-get -q install software-properties-common -y
-apt install build-essential zlib1g-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl -y
+
+apt-get -q install software-properties-common -y
+apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl -y
+
 # install system build tools needed by gridlabd
+
 apt-get -q install git -y
 apt-get -q install unzip -y
 apt-get -q install autoconf -y
@@ -16,7 +19,6 @@ apt-get -q install cmake -y
 apt-get -q install flex -y
 apt-get -q install bison -y
 apt-get -q install libcurl4-gnutls-dev -y
-apt-get -q install libncurses5-dev -y
 apt-get -q install subversion -y
 apt-get -q install util-linux -y
 apt-get install liblzma-dev -y
@@ -66,12 +68,12 @@ if [ ! -x /usr/bin/doxygen ]; then
 fi
 
 # # mono
-apt-get -q install curl -y
+
 if [ ! -f /usr/bin/mono ]; then
 	cd /tmp
-	apt install apt-transport-https dirmngr gnupg ca-certificates
+	apt install apt-transport-https dirmngr gnupg ca-certificates -y
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-	echo "deb https://download.mono-project.com/repo/debian stable-buster main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+	echo "deb https://download.mono-project.com/repo/debian stable-bustergrid main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 	apt-get -q update -y
 	apt-get -q install mono-devel -y
 fi
