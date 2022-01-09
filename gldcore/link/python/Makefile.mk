e9fef2fbba7c9ada80c6cc3a22f85bdf5643f2a2
David P. Chassin <david.chassin@me.com>
2022-01-09 15:42:14 -0800

    Fix build module installation

--- gldcore/link/python/Makefile.mk
+++ gldcore/link/python/Makefile.mk
@@ -1,5 +1,6 @@
 $(top_srcdir)/gldcore/link/python/dist/gridlabd-$(notdir $(prefix))-.tar.gz: gldcore/build.h
 	@echo "building $@"
+	@python -m pip install -y 1>/dev/null
 	@( export SRCDIR=$(realpath $(top_srcdir)) ; python3 -m build $(top_srcdir)/gldcore/link/python 1>/dev/null )
 
 python-install: $(top_srcdir)/gldcore/link/python/dist/gridlabd-$(notdir $(prefix))-.tar.gz
