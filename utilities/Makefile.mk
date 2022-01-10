ea7a4a33e18ccd3669b85e09a2e962046cf13aa1
David P. Chassin <dchassin@slac.stanford.edu>
2022-01-10 15:53:54 -0800

    Update syntax for sublime

--- utilities/Makefile.mk
+++ utilities/Makefile.mk
@@ -1,7 +1,8 @@
-SUBLIME_SYNTAX = $(HOME)/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/GridLAB-D.sublime-build
-SUBLIME_SOURCE = utilities/sublime/gridlabd.sublime-syntax
+SUBLIME_SYNTAX = $(HOME)/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/GLM.sublime-syntax
+SUBLIME_SOURCE = utilities/sublime/GLM.sublime-syntax
 
 sublime-syntax: $(SUBLIME_SYNTAX)
 
 $(SUBLIME_SYNTAX): $(SUBLIME_SOURCE)
-	cp $< "$@" || echo "ERROR: you need to install sublime package manager first"
+	@echo "Updating $(SUBLIME_SYNTAX)"
+	@cp $< "$@" || echo "ERROR: you need to install sublime package manager first"
