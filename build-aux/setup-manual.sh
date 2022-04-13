db6ac3c7a26260404cc307fb94496ba141875ea5
Catzzz <chuneleu@gmail.com>
2022-04-12 18:27:24 -0700

    update setup-manual.sh description

--- build-aux/setup-manual.sh
+++ build-aux/setup-manual.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cat <<-END
 This system has no automated installer.  You will have to run the installation manually and the specific commands
-will depend on how your system works.  See README.md for details.
+will depend on how your system works, or you can use --use-docker command to run build custom docker image in your system.  
+See README.md for details.
 END
