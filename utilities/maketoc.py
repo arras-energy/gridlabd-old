9e4873cf824ec56904d32f4944c1c46a9689fad0
David P. Chassin <dchassin@slac.stanford.edu>
2022-05-11 10:53:14 -0700

    Update TOC

--- utilities/maketoc.py
+++ utilities/maketoc.py
@@ -23,4 +23,4 @@ for CHAPTER in CHAPTERS:
 		spec = SECTION.split(" - ")
 		section = spec[0]
 		name = spec[1]
-		print("  ",f"{chapter}.{section}. [[{PATH}{CHAPTER}/{section} - {name.replace('.md','~')}]]","\n")
+		print("  ",f"{chapter}.[[{PATH}{CHAPTER}/{section} - {name.replace('.md','~')}]]","\n")
