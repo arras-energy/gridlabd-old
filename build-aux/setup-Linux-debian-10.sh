0432066e19c966509392768ad6dea9e06b1c7d7a
Thistleman <codes5096@gmail.com>
2022-06-28 12:10:19 -0700

    Fixed path for saved script

--- build-aux/setup-Linux-debian-10.sh
+++ build-aux/setup-Linux-debian-10.sh
@@ -74,6 +74,12 @@ if [ ! -x /usr/local/opt/gridlabd/bin/python3 -o "$(/usr/local/bin/python3 --ver
 	/usr/local/opt/gridlabd/bin/python3 -m pip install --upgrade pip
 	/usr/local/opt/gridlabd/bin/python3 -m pip install matplotlib Pillow pandas numpy networkx pytz pysolar PyGithub scikit-learn xlrd boto3
 	/usr/local/opt/gridlabd/bin/python3 -m pip install IPython censusdata
+
+	if [ ! -e /etc/ld.so.conf.d/gridlabd.conf ]; then
+		sudo touch /etc/ld.so.conf.d/gridlabd.conf
+		sudo bash -c 'echo "/usr/local/opt/gridlabd/lib" >> /etc/ld.so.conf.d/gridlabd.conf'
+		sudo ldconfig
+	fi
 fi
 
 # install latex
