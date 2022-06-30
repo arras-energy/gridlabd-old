1d9a151df08c5b3b12cea40eff96e684c9b6de4d
Thistleman <codes5096@gmail.com>
2022-06-30 15:43:19 -0700

    removed bugtest code

--- python/Makefile.mk
+++ python/Makefile.mk
@@ -3,16 +3,12 @@ PYTHONVERSION=$(shell python3 $(top_srcdir)/python/setup.py --version)
 $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION).tar.gz: $(top_srcdir)/source/build.h
 	@echo "building $@"
 	@rm -f $(top_srcdir)/python/dist/gridlabd-*.{whl,tar.gz}
-	echo "Build"
-	which python3
 	@python3 -m pip install build 1>/dev/null
 	@( export SRCDIR=$(realpath $(top_srcdir)) ; export BLDDIR=$(shell pwd); python3 -m build $(top_srcdir)/python 1>/dev/null )
 
 python-install: $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION).tar.gz
 
 	@echo "installing $@"
-	echo "Installz"
-	which python3
 	@python3 -m pip install --ignore-installed $(top_srcdir)/python/dist/gridlabd-*.whl
 
 python-clean:
