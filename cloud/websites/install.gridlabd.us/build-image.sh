a1b412c8c91cf1381ac8ba334e38430ad532cde9
Thistleman <codes5096@gmail.com>
2022-06-30 14:15:08 -0700

    updated build script

--- cloud/websites/install.gridlabd.us/build-image.sh
+++ cloud/websites/install.gridlabd.us/build-image.sh
@@ -3,15 +3,15 @@
 # This file is for developer and maintainer use only. 
 # Build gridlabd on a clean system for best results in building and saving an image.
 
-if [ ! -e ~/temp]; then
+if [ ! -e $HOME/temp ]; then
     echo "Home temp folder not found. Generating home temp folder."
-    mkdir ~/temp
-    cd ~/temp
+    mkdir $HOME/temp
+    cd $HOME/temp
     echo "Saving image in your home temp folder."
     tar -cjf gridlabd-youros-yourkernel-release-master.tar.bz2 -C /usr/local/opt .
     echo "Build complete. Don't forget to upload the image to aws!"
 else
-    cd ~/temp
+    cd $HOME/temp
     echo "Saving image in your home temp folder."
     tar -cjf gridlabd-youros-yourkernel-release-master.tar.bz2 -C /usr/local/opt .
     echo "Build complete. Don't forget to upload the image to aws!"
