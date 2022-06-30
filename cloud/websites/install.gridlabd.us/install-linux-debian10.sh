f0f40185ed9a2f8ef801d634acdec6e0951b3609
Thistleman <codes5096@gmail.com>
2022-06-30 14:17:54 -0700

    Updated variable in cloud install script

--- cloud/websites/install.gridlabd.us/install-linux-debian10.sh
+++ cloud/websites/install.gridlabd.us/install-linux-debian10.sh
@@ -3,17 +3,17 @@
 # This file is for developer and maintainer use only. 
 # Build gridlabd on a clean system for best results in building and saving an image.
 
-if [ ! -e ~/temp]; then
+if [ ! -e $HOME/temp]; then
     echo "Home temp folder not found. Generating home temp folder."
-    mkdir ~/temp
-    cd ~/temp
+    mkdir $HOME/temp
+    cd $HOME/temp
     echo "Downloading image to your home temp folder."
     wget https://s3.us-west-1.amazonaws.com/install-dev.gridlabd.us/debian-10-image.tar.bz2
     tar -xvf debian-10-image.tar.bz2
     sudo mv gridlabd /usr/local/opt
     echo "Gridlabd installed. Adding to path."
 else
-    cd ~/temp
+    cd $HOME/temp
     echo "Downloading image to your home temp folder."
     wget https://s3.us-west-1.amazonaws.com/install-dev.gridlabd.us/debian-10-image.tar.bz2
     tar -xvf debian-10-image.tar.bz2
