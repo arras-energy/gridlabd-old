32d44a637ed5136db36012bbbdb7f11f02d7090e
thistleman <codes5096@gmail.com>
2022-07-01 11:49:04 -0600

    Initial refactor of arm64 install script

--- build-aux/setup-Darwin-21-arm64.sh
+++ build-aux/setup-Darwin-21-arm64.sh
@@ -12,20 +12,11 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 # install homebrew instance for gridlabd
     brew update
     if test ! -e /opt/homebrew; then 
-        sudo mkdir /opt/homebrew 
-        sudo chmod 777 /opt/homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C /opt/homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
     export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
     brew update-reset
     brew doctor
-
-# linking bins with arm homebrew versions
-sudo ln -sf /opt/homebrew/bin /usr/local/bin
-sudo ln -sf /opt/homebrew/lib /usr/local/lib
-sudo ln -sf /opt/homebrew/include /usr/local/include
-sudo ln -sf /opt/homebrew/var /usr/local/var
-sudo ln -sf /opt/homebrew/etc /usr/local/etc
-sudo ln -sf /opt/homebrew/share /usr/local/share
 
 echo "$1"
 if ! grep -q "$1/bin" "$HOME/.zshrc"; then
@@ -49,21 +40,50 @@ fi
     [ ! -e /usr/local/bin/sed ] && ln -sf /opt/homebrew/bin/gsed /usr/local/bin/sed
     [ ! -e /usr/local/bin/libtoolize ] && ln -sf /opt/homebrew/bin/glibtoolize /usr/local/bin/libtoolize
 
-# install python3
-    brew install python3
+# Install python 3.9.6
+# python3 support needed as of 4.2
+if [ ! -x /usr/local/opt/gridlabd/bin/python3 -o "$(/usr/local/opt/gridlabd/bin/python3 --version | cut -f2 -d.)" != "Python 3.9" ]; then
+	echo "install python 3.9.6"
+	cd /usr/local/opt/gridlabd/src
+
+	curl https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz | tar xz
+	# tar xzf Python-3.9.6.tgz 
+	cd Python-3.9.6
+
+	./configure --prefix=/usr/local/opt/gridlabd --enable-optimizations --with-system-ffi --with-computed-gotos --enable-loadable-sqlite-extensions CFLAGS="-fPIC"
+
+	make -j $(nproc)
+	make install
+	/sbin/ldconfig /usr/local/opt/gridlabd/lib
+	ln -sf /usr/local/opt/gridlabd/bin/python3.9 /usr/local/opt/gridlabd/bin/python3
+	ln -sf /usr/local/opt/gridlabd/bin/python3.9-config /usr/local/opt/gridlabd/bin/python3-config
+	ln -sf /usr/local/opt/gridlabd/bin/pydoc3.9 /usr/local/opt/gridlabd/bin/pydoc
+	ln -sf /usr/local/opt/gridlabd/bin/idle3.9 /usr/local/opt/gridlabd/bin/idle
+	ln -sf /usr/local/opt/gridlabd/bin/pip3.9 /usr/local/opt/gridlabd/bin/pip3
+
+	/usr/local/opt/gridlabd/bin/python3 -m pip install --upgrade pip
+	/usr/local/opt/gridlabd/bin/python3 -m pip install matplotlib Pillow pandas numpy networkx pytz pysolar PyGithub scikit-learn xlrd boto3
+	/usr/local/opt/gridlabd/bin/python3 -m pip install IPython censusdata
+
+
+	cd $REQ_DIR
+	/usr/local/opt/gridlabd/bin/python3 -m pip install -r requirements.txt
+
+
+	if [ ! -e /etc/ld.so.conf.d/gridlabd.conf ]; then
+		sudo touch /etc/ld.so.conf.d/gridlabd.conf
+		sudo bash -c 'echo "/usr/local/opt/gridlabd/lib" >> /etc/ld.so.conf.d/gridlabd.conf'
+		sudo ldconfig
+	fi
+fi
 
 # mdbtools
     brew install mdbtools
-    pip3 install matplotlib pandas numpy networkx Pillow
 
 # The original scikit-learn at 0.24.2 CANNOT install on the m1 mac. Period. Use 1.1.1 now. Make sure requirements.txt has the change.
 # Reason being, is that it requires a version of NumPy that is incompatible with the m1 mac.
-# updated in requirements.txt. Same goes for scipy 1.6.2.
+# updated in requirements.txt. Same goes for scipy 1.6.2. Use 1.8.1.
     brew install gdal
-
-# Install pyproj manually due to error building final wheel
-    brew install proj
-    pip3 install pyproj
 
 # docs generators
     brew install mono
@@ -80,12 +100,5 @@ fi
     brew install svn
 
 # mysql connector
-    brew install mysql
-    brew install mysql-client
-
-sudo ln -sf /opt/homebrew/bin /usr/local/bin
-sudo ln -sf /opt/homebrew/lib /usr/local/lib
-sudo ln -sf /opt/homebrew/include /usr/local/include
-sudo ln -sf /opt/homebrew/var /usr/local/var
-sudo ln -sf /opt/homebrew/etc /usr/local/etc
-sudo ln -sf /opt/homebrew/share /usr/local/share
+#    brew install mysql
+#    brew install mysql-client
