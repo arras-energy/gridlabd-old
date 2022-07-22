c156028f0eb2d6a1270e9ebb55923f3288b6af87
Thistleman <codes5096@gmail.com>
2022-07-22 15:18:31 -0700

    Added base code for arm64 installs

--- cloud/websites/install.gridlabd.us/web-install.sh
+++ cloud/websites/install.gridlabd.us/web-install.sh
@@ -90,6 +90,88 @@ if test $D_ARCH != "arm64"; then
     # give user permissions for writing to site-packages
     sudo chown ${USER} /usr/local/opt/gridlabd/lib/python3.9/site-packages
     COMPAT_INSTALL=1
+
+# Code for arm64 installations
+else
+
+    # Darwin specific setup for necessary tools
+    if [ $SYSTEM == "Darwin"]; then
+        brew update
+        if test ! -e /opt/homebrew; then 
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        fi
+        export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
+        brew update-reset
+        brew doctor
+
+        brew install wget
+        # TODO - update more as needed
+    fi
+
+    if [ ! -e $HOME/temp ]; then
+        echo "Home temp folder not found. Generating home temp folder."
+        cd $HOME
+        mkdir temp
+        cd temp
+        echo "Downloading image to your home temp folder."
+        wget https://s3.us-west-1.amazonaws.com/install-dev.gridlabd.us/gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2
+
+        if [ ! -e $HOME/temp/gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2 ]; then
+            echo "A fast install image was not located for your operating system."
+            echo "You will need to build Gridlabd from source."
+            exit 1
+        fi
+        tar -xvf gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2
+
+
+        if [ ! -e /usr/local/opt ]; then
+            cd /usr/local
+            sudo mkdir opt
+        fi
+
+        cd $HOME/temp
+        sudo mv gridlabd /usr/local/opt
+        echo "Gridlabd installed. Adding to path."
+    else
+        cd $HOME/temp
+        echo "Downloading image to your home temp folder."
+        wget https://s3.us-west-1.amazonaws.com/install-dev.gridlabd.us/debian-10-image.tar.bz2
+        tar -xvf debian-10-image.tar.bz2
+
+        if [ ! -e /usr/local/opt ]; then
+            cd /usr/local
+            sudo mkdir opt
+        fi
+
+        cd $HOME/temp
+        sudo mv gridlabd /usr/local/opt
+        echo "Gridlabd installed. Adding to path."
+    fi
+
+    if ! grep -q "/usr/local/opt/gridlabd/bin" "$HOME/.bashrc"; then
+        touch "$HOME/.bashrc"
+        echo "export PATH=/usr/local/opt/gridlabd/bin:\$PATH" >> $HOME/.bashrc
+        sudo cp /usr/local/opt/gridlabd/bin/gridlabd /usr/local/bin
+        echo "Path saved to user's .bashrc file. If you use a different terminal than bash,"
+        echo "then you will need to manually add gridlabd to that terminal's startup file."
+    fi
+
+    # Linux specific installation customization
+    if [ $SYSTEM == "Linux"]; then
+        if [ ! -e /etc/ld.so.conf.d/gridlabd.conf ]; then
+            sudo touch /etc/ld.so.conf.d/gridlabd.conf
+            sudo bash -c 'echo "/usr/local/opt/gridlabd/lib" >> /etc/ld.so.conf.d/gridlabd.conf'
+            sudo ldconfig
+            echo "Added gridlabd lib to the dynamic loader library."
+        fi
+
+        # Add additional system requirements that cannot be saved in the image
+
+        echo "Ensuring additional system dependencies installed."
+        sudo apt-get install curl -y
+    fi
+
+    COMPAT_INSTALL=1
 fi
 
 if test COMPAT_INSTALL != 1; then
