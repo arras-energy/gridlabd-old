cbf966393bf7c66c5750ce019cf5b16353fced27
David P. Chassin <dchassin@slac.stanford.edu>
2022-07-28 14:41:24 -0700

    Update insights.py

--- tools/insights.py
+++ tools/insights.py
@@ -2,7 +2,7 @@
 
 SYNTAX
 
-  $ gridlabd insights CATEGORY [OPTIONS ...]
+	$ gridlabd insights CATEGORY [OPTIONS ...]
 
 DESCRIPTION
 
@@ -10,35 +10,57 @@ The `insights` utility retrieves gridlabd usage data from the AWS servers.
 
 The following options are available.
 
-  -c|--csv[=FILE]      Output result as CSV to stdout or FILE
+	-o|--output[=FILE]	 Output result as CSV to stdout or FILE
 
-  -d|--debug           Enable debugging (raises exception instead of printing errors)
+	-d|--debug					 Enable debugging (raises exception instead of printing errors)
 
-  -h|--help|help       Display this help
+	-h|--help|help			 Display this help
 
-  -m|--month=MONTH     Specify the month number (1-12)
+	-m|--month=MONTH		 Specify the month number (1-12)
 
-  -o|--output=CSVFILE  Specify the output CSV filename
+	-o|--output=CSVFILE	Specify the output CSV filename
 
-  -y|--year=YEAR       Specify the year number (4 digit year)
+	-y|--year=YEAR			 Specify the year number (4 digit year)
+
+  --signup						 Open the ipaddr signup page to get access token
+
+  --token=TOKEN				 Save the ipaddr access token (required for location category)
 
 CAVEAT
 
-Currently, only the category `requests` is supported. This returns the number of users
-who run gridlabd online each day of the month.
+Collects version usage data for HiPAS GridLAB-D by year and month.  If month
+is not specified, then all months of the current year are returned. If year
+is not specified, then all months of all years are returned.
+
+Currently, the supported categories are
+
+  requests: return a list of individual requests by year, month, day, and ipaddr.
+
+  location: return a list of request counts by year, month, day, location
 
 SEE ALSO
 
 * [[/Subcommand/Aws]]
 """
-import sys
+import sys, os
 import pandas
 import datetime
 import matplotlib.pyplot as plot
+import ipinfo
+
+try:
+	with open(f"{os.getenv('HOME')}/.ipinfo/access_token","r") as fh:
+		token = fh.read()
+except:
+	token = None
 
 today = datetime.datetime.now().date()
 URL = "http://version.gridlabd.us/access.csv"
 debug = False
+
+E_OK = 0
+E_INVALID = 1
+E_FAILED = 2
 
 def error(msg,code=None):
 	print(f"ERROR [insights]: {msg}",file=sys.stderr)
@@ -58,11 +80,6 @@ def syntax(code=None):
 
 def get_requests(year,month):
 
-	start = datetime.datetime.strptime(f'{year}-{month}-1+00:00','%Y-%m-%d%z')
-	if month==12:
-		stop = datetime.datetime.strptime(f'{year+1}-1-1+00:00','%Y-%m-%d%z')
-	else:
-		stop = datetime.datetime.strptime(f'{year}-{month+1}-1+00:00','%Y-%m-%d%z')
 	data = pandas.read_csv(URL,
 		names=['date','ipaddr','query','status','result'],
 		index_col=['status'],
@@ -70,14 +87,52 @@ def get_requests(year,month):
 		parse_dates=['date'],
 		error_bad_lines=False,
 		).loc['200'].reset_index().drop('status',axis=1)
-	data = data[data['date'] >= start]
-	data = data[data['date'] < stop]
+	if year or month:
+		if not month:
+			month = 1
+		elif not year:
+			year = today.year
+		start = datetime.datetime.strptime(f'{year}-{month}-1+00:00','%Y-%m-%d%z')
+		if month==12:
+			stop = datetime.datetime.strptime(f'{year+1}-1-1+00:00','%Y-%m-%d%z')
+		else:
+			stop = datetime.datetime.strptime(f'{year}-{month+1}-1+00:00','%Y-%m-%d%z')
+		data = data[data['date'] >= start]
+		data = data[data['date'] < stop]
+	data = data[data['query'].str.match(r'^GET /index.html\?check=')]
+	try:
+		data['version'] = data['query'].str.replace(r'^GET /index.html\?check=','')
+		data['version'] = data['version'].str.split(n=1,expand=True)
+		data = data[data['version']!="test"]
+	except:
+		pass
+	data.drop('query',axis=1,inplace=True)
+	data.drop('result',axis=1,inplace=True)
 	data['day'] = pandas.DatetimeIndex(data['date']).strftime('%Y-%m-%d')
 	data['month'] = pandas.DatetimeIndex(data['date']).strftime('%Y-%m')
 	data['year'] = pandas.DatetimeIndex(data['date']).strftime('%Y')
-	data.set_index(['month','ipaddr'],inplace=True)
+	return data.drop('date',axis=1).set_index(['year','month','day','ipaddr'])
 
-	return data['result'].groupby('ipaddr').count()
+def get_locations(year,month):
+	if not token:
+		error("no ipaddr access token found, use --signup to get one")
+	handler = ipinfo.getHandler(token)
+	data = get_requests(year,month)
+	result = data.groupby(data.index.names).count().reset_index()
+	ipaddr = list(result['ipaddr'])
+	info = handler.getBatchDetails(ipaddr)
+	country = []
+	region = []
+	city = []
+	for ip,specs in info.items():
+		country.append(specs['country'])
+		region.append(specs['region'])
+		city.append(specs['city'])
+	result['country'] = country
+	result['region'] = region
+	result['city'] = city
+	result.rename({"version":"count"},inplace=True)
+	return result.set_index(['year','month','day','ipaddr'])
 
 if __name__ == "__main__":
 	year = today.year
@@ -96,28 +151,61 @@ if __name__ == "__main__":
 			tag = arg
 			value = None
 		if tag in ["-y","--year"]:
-			year = int(value)
+			if year == None:
+				year = None
+			else:
+				year = int(value)
 		elif tag in ["-m","--month"]:
-			month = int(value)
+			if value == None:
+				month = None
+			else:
+				month = int(value)
 		elif tag in ["-d","--debug"]:
 			debug = True
 		elif tag in ["-o","--output"]:
 			output = value
 		elif tag in ["-h","--help","help"]:
 			syntax()
-		elif tag in ["-c","--csv"]:
+		elif tag in ["-0","--output"]:
 			if not value:
 				output = "/dev/stdout"
+		elif tag in ["--signup"]:
+			os.system("open https://ipinfo.io/signup")
+			print("save your token with --token=TOKEN option")
+			exit(0)
+		elif tag in ["--token"]:
+			try:
+				with open(f"{os.getenv('HOME')}/.ipinfo/access_token","w") as fh:
+					fh.write(value)
+			except:
+				e_type, e_value, e_trace = sys.exc_info()
+				error(f"{e_value} ({e_type.__name__})",E_FAILED)
+			exit(0)
 		elif "get_"+tag in globals().keys():
 			call = globals()["get_"+tag]
 		else:
-			error(f"{arg} is invalid",1)
+			error(f"{arg} is invalid",E_INVALID)
 
 	if not call:
 		error(f"insight category not specified",2)
 	else:
-		result = call(year,month)
-		if output:
-			result.to_csv(output)
-		else:
-			print(result.sort_index().reset_index())
+		try:
+			result = call(year,month).reset_index()
+			result.index.name = "id"
+			if output:
+				result.to_csv(output,index=True,header=True)
+			else:
+				pandas.options.display.max_rows = None
+				pandas.options.display.max_colwidth = None
+				pandas.options.display.max_columns = None
+				pandas.options.display.width = None
+				if len(result) > 0:
+					print(result)
+				else:
+					print("   "+" ".join(list(result.columns)))
+					print("id")
+		except:
+			if debug:
+				raise
+			e_type, e_value, e_trace = sys.exc_info()
+			error(f"{e_value} ({e_type.__name__})",E_FAILED)
