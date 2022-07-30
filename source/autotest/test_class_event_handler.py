8ae55d863235c3d554095d0fb0fb25402be54e6a
David P. Chassin <david.chassin@me.com>
2022-07-30 07:05:03 -0700

    Create test_class_event_handler.py

---
+++ source/autotest/test_class_event_handler.py
@@ -0,0 +1,41 @@
+import sys, gridlabd
+
+def on_init(t):
+    print(gridlabd.version(),file=sys.stderr)
+    return True
+
+def test_init(obj,t):
+    print(f"test_init(obj={obj},t={t})",file=sys.stderr)
+    gridlabd.set_value(obj,"n",gridlabd.get_value(obj,"n")+"1")
+    return False
+
+def test_precommit(obj,t):
+    print(f"test_precommit(obj={obj},t={t})",file=sys.stderr)
+    gridlabd.set_value(obj,"n",gridlabd.get_value(obj,"n")+"2")
+    return gridlabd.NEVER
+
+def test_presync(obj,t):
+    print(f"test_presync(obj={obj},t={t})",file=sys.stderr)
+    gridlabd.set_value(obj,"n",gridlabd.get_value(obj,"n")+"3")
+    return gridlabd.NEVER
+
+def test_sync(obj,t):
+    print(f"test_sync(obj={obj},t={t})",file=sys.stderr)
+    gridlabd.set_value(obj,"n",gridlabd.get_value(obj,"n")+"4")
+    return gridlabd.NEVER
+
+def test_postsync(obj,t):
+    print(f"test_postsync(obj={obj},t={t})",file=sys.stderr)
+    gridlabd.set_value(obj,"n",gridlabd.get_value(obj,"n")+"5")
+    return gridlabd.NEVER
+
+def test_commit(obj,t):
+    print(f"test_commit(obj={obj},t={t})",file=sys.stderr)
+    gridlabd.set_value(obj,"n",gridlabd.get_value(obj,"n")+"6")
+    return gridlabd.NEVER
+
+def test_finalize(obj,t):
+    print(f"test_finalize(obj={obj},t={t})",file=sys.stderr)
+    gridlabd.set_value(obj,"n",gridlabd.get_value(obj,"n")+"7")
+    return gridlabd.NEVER
+
