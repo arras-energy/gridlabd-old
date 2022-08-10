ea51178a30915318ff663d76dfa91ad9e52bd768
thistleman <codes5096@gmail.com>
2022-08-10 15:06:51 -0700

    updated install.sh

--- cloud/websites/install.gridlabd.us/install.sh
+++ cloud/websites/install.gridlabd.us/install.sh
@@ -1,119 +1,248 @@
 #!/bin/bash
 
-[ -z "$SOURCE" ] && SOURCE=http://install.gridlabd.us
+# Gather system information for correct install
 
-function error()
-{
-    RC=$1
-    shift 1
-    echo "ERROR [$SOURCE]: $*"
-    exit $RC
-}
+SYSTEM=$(uname -s)
+RELEASE=$(uname -r | cut -f1 -d.)
+KERNEL=
+if test $SYSTEM == "Linux"; then
+source "/etc/os-release"
+KERNEL="-$ID-$(echo $VERSION_ID | cut -f1 -d.)"
+L_DISTRO="$ID"
+fi
+D_ARCH=$(uname -m)
 
-function warning()
-{
-    echo "WARNING [$SOURCE]: $*"
-}
-
-function url_ok ()
-{
-    RESULT=$(curl -sI $1 | head -n 1)
-    HTTPVER=$(echo "$RESULT" | cut -f1 -d' ')
-    CODE=$(echo "$RESULT" | cut -f2 -d' ')
-    if [ "$HTTPVER" == "HTTP/1.1" ]; then
-        if [ "$CODE" -eq "200" ]; then
-            return 0
-        else
-            return $CODE
-        fi
-    else
-        return -1
-    fi
-}
-
-function url_err ()
-{
-    TEXT=$(echo "$RESULT" | cut -f3- -d' ' | tr -dc A-Za-z\ )
-    if [ "$HTTPVER" == "HTTP/1.1" ]; then
-        if [ "$CODE" -ne "200" ]; then
-            error 1 "URL '$1'; $TEXT (error $CODE)"
-            return 1
-        fi
-    else
-        error 2 "'curl -sI $1' failed"
-    fi
-}
-
-function url_run ()
-{
-    url_ok "$1" && ( curl -sL "$1" | /bin/bash ) || return 1
-}
-
-function url_tarxz ()
-{
-    url_ok "$1" && ( curl -sL "$1" | tar xz ) || url_err "$1"
-}
-
-SYSTEM=$(uname -s | tr A-Z a-z)
-case $SYSTEM in
-
-    darwin)
-        
-        
-        RELEASE=$(uname -r | cut -f1 -d.)
-        alias nproc="sysctl -n hw.ncpu"
-
-        ;;
-
-    linux)
-
-        source "/etc/os-release"
-        RELEASE=$ID$VERSION_ID
-
-        ;;
-
-    *)
-
-        error 3 "install not available for $SYSTEM, try manual installation instead"
-
-        ;;
-
-esac
-
-url_ok $SOURCE/latest-$SYSTEM-$RELEASE.txt || error 4 "unable to get $SOURCE/latest-$SYSTEM-$RELEASE.txt"
-LATEST=$(curl -sL $SOURCE/latest-$SYSTEM-$RELEASE.txt)
-
-TARGET=/usr/local/opt/gridlabd
-mkdir -p $TARGET
-cd $TARGET
-
-if ( url_run "$SOURCE/install-$SYSTEM-$RELEASE.sh" && url_tarxz "$SOURCE/gridlabd-$SYSTEM-$RELEASE-${LATEST%-*}-master.tarz" && /usr/local/opt/gridlabd/$LATEST-master/bin/gridlabd version set ); then
-    echo -n ""
-else
-    echo "fast install failed, running full build"
-    mkdir -p /usr/local/src
-    cd /usr/local/src
-    git clone https://github.com/slacgismo/gridlabd --depth 1 gridlabd
-    cd gridlabd
-    autoreconf -isf
-    ./configure
-    export MAKEFLAGS=-j$(($(nproc)*3))
-    export PYTHONSETUPFLAGS="-j $(($(nproc)*3))"
-    make system
+if test ! -e /usr/local/bin; then
+    cd /usr/local
+    sudo mkdir bin
 fi
 
-echo '
-**************************************************
-Welcome to the HiPAS GridLAB-D host.
+# Standard universal image install for non-arm systems
 
-Use 'gridlabd --help' for help on using GridLAB-D
+if test $D_ARCH != "arm64"; then
 
-Online documentation is available at
+    # Darwin specific setup for necessary tools
+    if [ $SYSTEM == "Darwin" ]; then
+        brew update || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        brew doctor
 
-  https://docs.gridlabd.us/
+        brew install wget
+    fi
 
-**************************************************
-' > /etc/motd
-echo 'export PATH=/usr/local/bin:$PATH' >> /etc/bashrc
+    if [ ! -e $HOME/temp ]; then
+        echo "Home temp folder not found. Generating home temp folder."
+        cd $HOME
+        mkdir temp
+        cd temp
+        echo "Downloading image to your home temp folder."
+        wget https://s3.us-west-1.amazonaws.com/install-dev.gridlabd.us/gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2
 
-[ -z "$(which gridlabd 2>/dev/null)" ] && echo "gridlabd will be in your PATH the next time you log in"
+        if [ ! -e $HOME/temp/gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2 ]; then
+            echo "A fast install image was not located for your operating system."
+            echo "You will need to build Gridlabd from source."
+            exit 1
+        fi
+        tar -xvf gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2
+
+
+        if [ ! -e /usr/local/opt ]; then
+            cd /usr/local
+            sudo mkdir opt
+        fi
+
+        cd $HOME/temp
+        sudo mv gridlabd /usr/local/opt
+        echo "Gridlabd installed. Adding to path."
+    else
+        cd $HOME/temp
+        echo "Downloading image to your home temp folder."
+        wget https://s3.us-west-1.amazonaws.com/install-dev.gridlabd.us/gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2
+        tar -xvf gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2
+
+        if [ ! -e /usr/local/opt ]; then
+            cd /usr/local
+            sudo mkdir opt
+        fi
+
+        cd $HOME/temp
+        sudo mv gridlabd /usr/local/opt
+        echo "Gridlabd installed. Adding to path."
+    fi
+
+    if ! grep -q "/usr/local/opt/gridlabd/bin" "$HOME/.bashrc"; then
+        touch "$HOME/.bashrc"
+        echo "export PATH=/usr/local/opt/gridlabd/bin:\$PATH" >> $HOME/.bashrc
+        sudo cp /usr/local/opt/gridlabd/bin/gridlabd /usr/local/bin
+        echo "Path saved to user's .bashrc file. If you use a different terminal than bash,"
+        echo "then you will need to manually add gridlabd to that terminal's startup file."
+    fi
+
+    # Linux specific installation customization
+    if [ $SYSTEM == "Linux" ]; then
+        if [ ! -e /etc/ld.so.conf.d/gridlabd.conf ]; then
+            sudo touch /etc/ld.so.conf.d/gridlabd.conf
+            sudo bash -c 'echo "/usr/local/opt/gridlabd/lib" >> /etc/ld.so.conf.d/gridlabd.conf'
+            sudo ldconfig
+            echo "Added gridlabd lib to the dynamic loader library."
+        fi
+
+        # Add additional system requirements that cannot be saved in the image
+
+        echo "Ensuring additional system dependencies installed."
+        sudo apt-get install curl -y
+        sudo apt-get install libgdal-dev -y
+
+        if [ $L_DISTRO == "Debian" ]; then
+            sudo apt-get install g++ -y
+        fi
+
+        if [ $L_DISTRO == "Ubuntu" ]; then
+            sudo apt-get install libgeos-dev -y
+        fi
+
+    fi
+
+    # give user permissions for writing to site-packages
+    sudo chown ${USER} /usr/local/opt/gridlabd/lib/python3.9/site-packages
+
+    # Add symlink for binary to /usr/local/bin
+    sudo ln -sf /usr/local/opt/gridlabd/bin/gridlabd* /usr/local/bin
+
+# Code for arm64 installations
+else
+
+    # Darwin specific setup for necessary tools
+    if [ $SYSTEM == "Darwin" ]; then
+        brew update
+        if test ! -e /opt/homebrew; then 
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        fi
+        export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
+        brew update-reset
+        brew doctor
+
+        brew install wget
+        # TODO - update more as needed
+    fi
+
+    if [ ! -e $HOME/temp ]; then
+        echo "Home temp folder not found. Generating home temp folder."
+        cd $HOME
+        mkdir temp
+        cd temp
+        echo "Downloading image to your home temp folder."
+        wget https://s3.us-west-1.amazonaws.com/install-dev.gridlabd.us/gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2
+
+        if [ ! -e $HOME/temp/gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2 ]; then
+            echo "A fast install image was not located for your operating system."
+            echo "You will need to build Gridlabd from source."
+            exit 1
+        fi
+        tar -xvf gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2
+
+
+        if [ ! -e /usr/local/opt ]; then
+            cd /usr/local
+            sudo mkdir opt
+        fi
+
+        cd $HOME/temp
+        sudo mv gridlabd /usr/local/opt
+        echo "Gridlabd installed. Adding to path."
+    else
+        cd $HOME/temp
+        echo "Downloading image to your home temp folder."
+        wget https://s3.us-west-1.amazonaws.com/install-dev.gridlabd.us/gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2
+        tar -xvf gridlabd-$SYSTEM$KERNEL-$RELEASE-$D_ARCH-master.tar.bz2
+
+        if [ ! -e /usr/local/opt ]; then
+            cd /usr/local
+            sudo mkdir opt
+        fi
+
+        cd $HOME/temp
+        sudo mv gridlabd /usr/local/opt
+        echo "Gridlabd installed. Adding to path."
+    fi
+
+    if ! grep -q "/usr/local/opt/gridlabd/bin" "$HOME/.bashrc"; then
+        touch "$HOME/.bashrc"
+        echo "export PATH=/usr/local/opt/gridlabd/bin:\$PATH" >> $HOME/.bashrc
+        sudo cp /usr/local/opt/gridlabd/bin/gridlabd /usr/local/bin
+        echo "Path saved to user's .bashrc file. If you use a different terminal than bash,"
+        echo "then you will need to manually add gridlabd to that terminal's startup file."
+    fi
+
+    # Linux specific installation customization
+    if [ $SYSTEM == "Linux" ]; then
+        if [ ! -e /etc/ld.so.conf.d/gridlabd.conf ]; then
+            sudo touch /etc/ld.so.conf.d/gridlabd.conf
+            sudo bash -c 'echo "/usr/local/opt/gridlabd/lib" >> /etc/ld.so.conf.d/gridlabd.conf'
+            sudo ldconfig
+            echo "Added gridlabd lib to the dynamic loader library."
+        fi
+
+        # Add additional system requirements that cannot be saved in the image
+
+        echo "Ensuring additional system dependencies installed."
+        sudo apt-get install curl -y
+        sudo apt-get install libgdal-dev -y
+
+        if [ $L_DISTRO == "Debian" ]; then
+            sudo apt-get install g++ -y
+        fi
+        
+        if [ $L_DISTRO == "Ubuntu" ]; then
+            sudo apt-get install libgeos-dev -y
+        fi
+    fi
+
+    # give user permissions for writing to site-packages
+    sudo chown ${USER} /usr/local/opt/gridlabd/lib/python3.9/site-packages
+
+    # Add symlink for binary to /usr/local/bin
+    sudo ln -sf /usr/local/opt/gridlabd/bin/gridlabd* /usr/local/bin
+
+fi
+
+# Darwin can vary a lot in how their shells add to path. So just hit them all. 
+if [ $SYSTEM == "Darwin" ]; then
+
+    if ! grep -q "/usr/local/opt/gridlabd/bin" "$HOME/.zshrc"; then
+        touch "$HOME/.zshrc"
+        echo "export PATH=/usr/local/opt/gridlabd/bin:\$PATH" >> $HOME/.zshrc
+    fi
+
+    if ! grep -q "/usr/local/opt/gridlabd/bin" "$HOME/.zsh_profile"; then
+        touch "$HOME/.zsh_profile"
+        echo "export PATH=/usr/local/opt/gridlabd/bin:\$PATH" >> $HOME/.zsh_profile
+    fi
+
+    if ! grep -q "/usr/local/opt/gridlabd/bin" "$HOME/.bash_profile"; then
+        touch "$HOME/.bash_profile"
+        echo "export PATH=/usr/local/opt/gridlabd/bin:\$PATH" >> $HOME/.bash_profile
+    fi
+
+    if ! grep -q "/usr/local/opt/gridlabd/lib" "$HOME/.bashrc"; then
+        touch "$HOME/.bashrc"
+        echo "export PATH=/usr/local/opt/gridlabd/bin:\$PATH" >> $HOME/.bashrc
+    fi    
+fi
+
+# link all libraries from package to /usr/local/lib, necessary for darwin and specific packages are hardcoded.
+# makes life easier this way.  
+if test ! -e /usr/local/lib; then
+    cd /usr/local
+    sudo mkdir lib
+fi
+
+sudo ln -s /usr/local/opt/gridlabd/lib/lib* /usr/local/lib 
+
+cd $HOME/temp
+sudo rm -rf gridlabd*
+
+if [ ! -e /usr/local/opt/gridlabd/bin/gridlabd ]; then
+            echo "A fast install image was not located for your operating system."
+            echo "You will need to build Gridlabd from source."
+            exit 1
+fi
