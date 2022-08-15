4b9eb6684a625c63f11f720fe47d8f505c01ff03
Thistleman <codes5096@gmail.com>
2022-08-15 09:26:47 -0700

    Updated Darwin 20 install for testing

--- build-aux/setup-Darwin-20.sh
+++ build-aux/setup-Darwin-20.sh
@@ -1,17 +1,130 @@
 #!/bin/bash
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+export PATH=/usr/local/opt/gridlabd/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 brew update || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/HomeBrew/install/master/install)"
 brew doctor
 
-# build tools
-brew install autoconf automake libtool gnu-sed gawk
-[ ! -L /usr/local/bin/sed -o ! "$(readlink /usr/local/bin/sed)" == "/usr/local/bin/gsed" ] && mv /usr/local/bin/sed /usr/local/bin/sed-old
-[ ! -e /usr/local/bin/sed ] && ln -s /usr/local/bin/gsed /usr/local/bin/sed
-[ ! -e /usr/local/bin/libtoolize ] && ln -s /usr/local/bin/glibtoolize /usr/local/bin/libtoolize
+# checking for necessary directories
 
-# install python3
-brew install python3
+    if test ! -e /usr/local/bin; then 
+        cd /usr/local
+        sudo mkdir bin
+    fi
+
+    if test ! -e /usr/local/lib; then 
+        cd /usr/local
+        sudo mkdir lib
+    fi
+
+    if test ! -e /usr/local/etc; then 
+        cd /usr/local
+        sudo mkdir etc
+    fi
+
+# adding necessary paths to user bash and zsh terminals
+# apparently, which profile or rc file is used varies wildly across Macs. RIP me. Add to all. =')
+if ! grep -q "/usr/local/opt/gridlabd/bin" "$HOME/.zshrc"; then
+    touch "$HOME/.zshrc"
+    echo "export PATH=/usr/local/opt/gridlabd/bin:\$PATH" >> $HOME/.zshrc
+    echo "export DYLD_LIBRARY_PATH=/usr/local/opt/gridlabd/lib:\$DYLD_LIBRARY_PATH" >> $HOME/.zshrc
+fi
+
+if ! grep -q "/usr/local/opt/gridlabd/bin" "$HOME/.zsh_profile"; then
+    touch "$HOME/.zsh_profile"
+    echo "export PATH=/usr/local/opt/gridlabd/bin:\$PATH" >> $HOME/.zsh_profile
+    echo "export DYLD_LIBRARY_PATH=/usr/local/opt/gridlabd/lib:\$DYLD_LIBRARY_PATH" >> $HOME/.zsh_profile
+fi
+
+if ! grep -q "/usr/local/opt/gridlabd/bin" "$HOME/.bash_profile"; then
+    touch "$HOME/.bash_profile"
+    echo "export PATH=/usr/local/opt/gridlabd/bin:\$PATH" >> $HOME/.bash_profile
+    echo "export DYLD_LIBRARY_PATH=/usr/local/opt/gridlabd/lib:\$DYLD_LIBRARY_PATH" >> $HOME/.bash_profile
+    echo "export LD_LIBRARY_PATH=/usr/local/opt/gridlabd/lib:\$LD_LIBRARY_PATH" >> $HOME/.bash_profile
+    echo "export LIBRARY_PATH=/usr/local/opt/gridlabd/lib:\$LIBRARY_PATH" >> $HOME/.bash_profile
+fi
+
+if ! grep -q "/usr/local/opt/gridlabd/lib" "$HOME/.bashrc"; then
+    touch "$HOME/.bashrc"
+    echo "export PATH=/usr/local/opt/gridlabd/bin:\$PATH" >> $HOME/.bashrc
+    echo "export DYLD_LIBRARY_PATH=/usr/local/opt/gridlabd/lib:\$DYLD_LIBRARY_PATH" >> $HOME/.bashrc
+    echo "export LD_LIBRARY_PATH=/usr/local/opt/gridlabd/lib:\$LD_LIBRARY_PATH" >> $HOME/.bashrc
+    echo "export LIBRARY_PATH=/usr/local/opt/gridlabd/lib:\$LIBRARY_PATH" >> $HOME/.bashrc
+fi
+
+export DYLD_LIBRARY_PATH=/usr/local/opt/gridlabd/lib:/opt/homebrew/lib:$DYLD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=/usr/local/opt/gridlabd/lib:/opt/homebrew/lib:$DYLD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/opt/gridlabd/lib:/opt/homebrew/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/opt/gridlabd/lib:/opt/homebrew/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/usr/local/opt/gridlabd/lib:/opt/homebrew/lib:$LIBRARY_PATH
+export LIBRARY_PATH=/usr/local/opt/gridlabd/lib:/opt/homebrew/lib:$LIBRARY_PATH
+
+# build tools
+
+    brew install autoconf automake libtool gnu-sed gawk git
+    brew install libffi zlib
+    brew install pkg-config openssl@1.1 xz gdbm tcl-tk
+    xcode-select --install
+
+    cd /usr/local
+    sudo mkdir ssl
+    sudo ln -sf /opt/homebrew/opt/openssl /usr/local/ssl
+    # Update symlinks in /usr/local/bin
+    #if [ ! -L "/usr/local/bin" ] && [ -d "/usr/local/bin" ]; then
+        #mv /usr/local/bin/* /opt/homebrew/bin
+        #sudo ln -sf /opt/homebrew/bin /usr/local/bin
+    #fi
+    [ ! -e /usr/local/bin/sed ] && sudo ln -sf /opt/homebrew/bin/gsed /usr/local/bin/sed
+    [ ! -e /usr/local/bin/libtoolize ] && sudo ln -sf /opt/homebrew/bin/glibtoolize /usr/local/bin/libtoolize
+    [ ! -e /usr/local/bin/libtool ] && sudo ln -sf /opt/homebrew/bin/glibtool /usr/local/bin/libtool
+
+# Install python 3.9.13
+# python3 support needed as of 4.2
+if [ ! -x /usr/local/opt/gridlabd/bin/python3 -o "$(/usr/local/opt/gridlabd/bin/python3 --version | cut -f3 -d.)" != "Python 3.9" ]; then
+	echo "install python 3.9.13"
+	cd /usr/local/opt/gridlabd/src
+
+	curl https://www.python.org/ftp/python/3.9.13/Python-3.9.13.tgz | tar xz
+	# tar xzf Python-3.9.13.tgz 
+	cd Python-3.9.13
+
+    export MACOSX_DEPLOYMENT_TARGET=19.6
+    export PKG_CONFIG_PATH="/usr/local/opt/gridlabd/lib/pkgconfig:$pythonLocation/lib/pkgconfig"
+	./configure --prefix=/usr/local/opt/gridlabd \
+    --enable-framework=/usr/local/opt/gridlabd \
+    --with-openssl=/opt/homebrew/opt/openssl@1.1 \
+    --with-pydebug \
+    --with-computed-gotos \
+    --with-tcltk-libs="$(pkg-config --libs tcl tk)" \
+    --with-tcltk-includes="$(pkg-config --cflags tcl tk)"
+
+	make -s -j2
+	make install 
+
+	sudo ln -sf /usr/local/opt/gridlabd/bin/python3.9 /usr/local/opt/gridlabd/bin/python3
+	sudo ln -sf /usr/local/opt/gridlabd/bin/python3.9-config /usr/local/opt/gridlabd/bin/python3-config
+	sudo ln -sf /usr/local/opt/gridlabd/bin/pydoc3.9 /usr/local/opt/gridlabd/bin/pydoc
+	sudo ln -sf /usr/local/opt/gridlabd/bin/idle3.9 /usr/local/opt/gridlabd/bin/idle
+	sudo ln -sf /usr/local/opt/gridlabd/bin/pip3.9 /usr/local/opt/gridlabd/bin/pip3
+    # macos refuses to let me set my all-important library paths. I have to link to /usr/local/lib otherwise the libraries cannot be found.
+    # I also need to now add an os-specific script section to the cloud install script. Thanks, apple.
+    #if ! test -e /usr/local/lib; then 
+    #    sudo mkdir /usr/local/lib
+    #    sudo ln -sf /usr/local/opt/gridlabd/lib/* /usr/local/lib
+    #else
+    #sudo ln -s /usr/local/opt/gridlabd/lib/* /usr/local/lib
+    #fix
+	/usr/local/opt/gridlabd/bin/python3 -m pip install --upgrade pip
+	/usr/local/opt/gridlabd/bin/python3 -m pip install matplotlib Pillow pandas numpy networkx pytz pysolar PyGithub scikit-learn xlrd boto3
+    /usr/local/opt/gridlabd/bin/python3 -m pip install build
+    /usr/local/opt/gridlabd/bin/python3 -m pip install pyproj
+
+    sudo ln -s /usr/local/opt/gridlabd/Python.framework/Versions/Current/bin/* /usr/local/opt/gridlabd/bin
+    sudo ln -s /usr/local/opt/gridlabd/Python.framework/Versions/Current/include/* /usr/local/opt/gridlabd/include
+    sudo ln -s /usr/local/opt/gridlabd/Python.framework/Versions/Current/lib/* /usr/local/opt/gridlabd/lib
+    sudo ln -s /usr/local/opt/gridlabd/Python.framework/Versions/Current/share/* /usr/local/opt/gridlabd/share
+fi
+
+brew install gdal
 
 # mdbtools
 brew install mdbtools
