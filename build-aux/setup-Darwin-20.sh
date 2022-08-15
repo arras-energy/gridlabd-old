beecf4c7e4e63b175f02b12f4867f68b933aa5e8
Thistleman <codes5096@gmail.com>
2022-08-15 14:50:44 -0700

    potential fix for ssl issues

--- build-aux/setup-Darwin-20.sh
+++ build-aux/setup-Darwin-20.sh
@@ -68,6 +68,7 @@ export LIBRARY_PATH=/usr/local/opt/gridlabd/lib:$LIBRARY_PATH
     cd /usr/local
     sudo mkdir ssl
     sudo ln -sf /usr/local/opt/openssl* /usr/local/ssl
+    sudo ln -sf /usr/local/ssl/openssl@1.1/include/* /usr/local/ssl
     # Update symlinks in /usr/local/bin
     #if [ ! -L "/usr/local/bin" ] && [ -d "/usr/local/bin" ]; then
         #mv /usr/local/bin/* /opt/homebrew/bin
