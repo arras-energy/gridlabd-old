019b7ce4b9cf07d2feeb075d2ee90e94455e1ae6
Thistleman <codes5096@gmail.com>
2022-08-24 18:53:49 -0700

    adjusted additional variables

--- python/Makefile.mk
+++ python/Makefile.mk
@@ -4,15 +4,15 @@ $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION).tar.gz: $(top_srcdir)/source
 
 	@echo "building $@"
 	@rm -f $(top_srcdir)/python/dist/gridlabd-*.{whl,tar.gz}
-	@python3 -m pip install build 1>/dev/null
+	$(bindir)/python3 -m pip install build 1>/dev/null
 	@( export SRCDIR=$(realpath $(top_srcdir)) ; export BLDDIR=$(shell pwd); $(bindir)/python3 -m build $(top_srcdir)/python 1>/dev/null )
 
 python-install: $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION).tar.gz
 
 	@echo "installing $@"
-	@python3 -m pip install --ignore-installed $(top_srcdir)/python/dist/gridlabd-*.whl
+	$(bindir)/python3 -m pip install --ignore-installed $(top_srcdir)/python/dist/gridlabd-*.whl
 
 python-clean:
 	@rm $(top_srcdir)/python/dist/*.{whl,tar.gz}
 	@echo "uninstalling $(top_srcdir)/python"
-	@python3 -m pip uninstall gridlabd -y || (echo "Use '. utilities/cleanwc' instead to clean this build."; exit 1)
+	$(bindir)/python3 -m pip uninstall gridlabd -y || (echo "Use '. utilities/cleanwc' instead to clean this build."; exit 1)
