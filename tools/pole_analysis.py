19b6804fb4e879b302277be47c16343d35598a7a
Fuhong Xie <fxie2@slac.stanford.edu>
2022-08-26 14:19:25 -0600

    Update pole_analysis.py

--- tools/pole_analysis.py
+++ tools/pole_analysis.py
@@ -210,7 +210,6 @@ def reactions_PW(g_diameter,t_diameter,height,density,tilt_angle,tilt_direction)
                     *(g_diameter**2+g_diameter*t_diameter+t_diameter**2)*height/(3*4)
     xV = -pole_weight*cos(pi*tilt_direction/180)
     yV = -pole_weight*sin(pi*tilt_direction/180)
-    (g_diameter/(g_diameter-t_diameter)-height*sqrt(g_diameter/(2*(g_diameter-t_diameter))))
     return xV, yV
 
 def reactions_CT(strength,tilt_angle,heading_direction):
