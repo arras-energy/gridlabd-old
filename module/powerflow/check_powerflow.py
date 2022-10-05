b26e1c26c6d05969042477e47319704c88385dbf
David P. Chassin <dchassin@slac.stanford.edu>
2022-10-05 15:37:05 -0700

    Fix exception

--- module/powerflow/check_powerflow.py
+++ module/powerflow/check_powerflow.py
@@ -1,17 +1,21 @@
 def check(context,model,options={}):
+	"""Verify a powerflow network model"""
 	count = 0
 	classes = []
 	for name, values in model["classes"].items():
-		if "module" in values.keys() and values["module"] == __name__.replace("check_",""):
+		if "module" in values and values["module"] == __name__.replace("check_",""):
 			classes.append(name)
 	objects = {}
 	for name, values in model["objects"].items():
-		if "class" in values.keys() and values["class"] in classes:
+		if "class" in values and values["class"] in classes:
 			objects[name] = values
-	count += check_swing(context,objects)
+	for method in globals():
+		if method.startswith("check_"):
+			count += globals()[method](context,objects)
 	return count
 
 def check_swing(context,objects):
+	"""Verify that one and only one swing bus is present"""
 	swing = []
 	for name,values in objects.items():
 		oclass = values["class"]
@@ -25,5 +29,27 @@ def check_swing(context,objects):
 		return 1
 	elif len(swing) > 1:
 		context.warning(f"powerflow network has {len(swing)} swing busses ({', '.join(swing)})")
-		return 1
+		return len(swing)
 	return 0
+
+def check_parallel(context,objects):
+	"""Verify that no two nodes are connected by more than one link"""
+	links = {}
+	count = 0
+	for name,values in objects.items():
+		if "from" in values and "to" in values:
+			n = values["from"]
+			m = values["to"]
+			if n < m:
+				link = f"{n}--{m}"
+			elif m < n:
+				link = f"{m}--{n}"
+			else:
+				context.warning(f"link '{name}' is circular")
+				count += 1
+			if link in links:
+				context.warning(f"links '{name}' and '{links[link]}' are parallel")
+				count += 1
+			else:
+				links[link] = name
+	return count
