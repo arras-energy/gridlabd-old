53ef546df421eff2363b27817b7a2387ea635d8a
Thistleman <codes5096@gmail.com>
2022-10-19 21:24:38 -0700

    adjustment

--- source/daemon.cpp
+++ source/daemon.cpp
@@ -21,9 +21,9 @@ static char pidfile[1024] = "/tmp/gridlabd-pid";
 static char workdir[1024] = "/tmp";
 #else
 static char user[1024] = "gridlabd";
-static char logfile[1024] = {std::getenv("GLD_VAR") + std::string("/gridlabd/gridlabd-log")};
-static char pidfile[1024] = {std::getenv("GLD_VAR") + std::string("/gridlabd/gridlabd-pid")};
-static char workdir[1024] = {std::getenv("GLD_VAR") + std::string("/gridlabd")};
+static char logfile[1024] = std::getenv("GLD_VAR") + std::string("/gridlabd/gridlabd-log");
+static char pidfile[1024] = std::getenv("GLD_VAR") + std::string("/gridlabd/gridlabd-pid");
+static char workdir[1024] = std::getenv("GLD_VAR") + std::string("/gridlabd");
 #endif
 
 // gridlabd stream specifications
