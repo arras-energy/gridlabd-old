b024c3f874dcf3889e3c200ffe5d639401d4eac9
thistleman <codes5096@gmail.com>
2023-03-21 08:32:15 -0700

    test using system python

--- build-aux/setup-Linux-debian-11.sh
+++ build-aux/setup-Linux-debian-11.sh
@@ -94,28 +94,28 @@ sudo apt-get install curl -y
 # Install python $PYTHON_VER
 # python3 support needed as of 4.2
 if [ ! -x $VERSION_DIR/bin/python3 -o "$($VERSION_DIR/bin/python3 --version | cut -f3 -d.)" != "Python $PY_EXE" ]; then
-	echo "installing python $PYTHON_VER and ssl module dependencies"
-	cd $VERSION_DIR/src
+#	echo "installing python $PYTHON_VER and ssl module dependencies"
+#	cd $VERSION_DIR/src
 
-	curl https://www.python.org/ftp/python/$PYTHON_VER/Python-$PYTHON_VER.tgz | tar xz
+#	curl https://www.python.org/ftp/python/$PYTHON_VER/Python-$PYTHON_VER.tgz | tar xz
 
-	# tar xzf Python-$PYTHON_VER.tgz
-	cd $VERSION_DIR/src/Python-$PYTHON_VER
+#	# tar xzf Python-$PYTHON_VER.tgz
+#	cd $VERSION_DIR/src/Python-$PYTHON_VER
 
-	./configure --prefix=$VERSION_DIR --enable-shared --enable-optimizations --with-system-ffi --with-computed-gotos --enable-loadable-sqlite-extensions CFLAGS="-fPIC"
+#	./configure --prefix=$VERSION_DIR --enable-shared --enable-optimizations --with-system-ffi --with-computed-gotos --enable-loadable-sqlite-extensions CFLAGS="-fPIC"
 
-	make -j $(nproc)
-	make install
-	/sbin/ldconfig $VERSION_DIR/lib
-	ln -sf $VERSION_DIR/bin/python${PY_EXE} $VERSION_DIR/bin/python3
-	ln -sf $VERSION_DIR/bin/python${PY_EXE}-config $VERSION_DIR/bin/python3-config
-	ln -sf $VERSION_DIR/bin/pydoc${PY_EXE} $VERSION_DIR/bin/pydoc
-	ln -sf $VERSION_DIR/bin/idle${PY_EXE} $VERSION_DIR/bin/idle
-	ln -sf $VERSION_DIR/bin/pip${PY_EXE} $VERSION_DIR/bin/pip3
+#	make -j $(nproc)
+#	make install
+#	/sbin/ldconfig $VERSION_DIR/lib
+#	ln -sf $VERSION_DIR/bin/python${PY_EXE} $VERSION_DIR/bin/python3
+#	ln -sf $VERSION_DIR/bin/python${PY_EXE}-config $VERSION_DIR/bin/python3-config
+#	ln -sf $VERSION_DIR/bin/pydoc${PY_EXE} $VERSION_DIR/bin/pydoc
+#	ln -sf $VERSION_DIR/bin/idle${PY_EXE} $VERSION_DIR/bin/idle
+#	ln -sf $VERSION_DIR/bin/pip${PY_EXE} $VERSION_DIR/bin/pip3
 
-	ln -sf $VERSION_DIR/bin/python${PY_EXE}-config $VAR/$VERSION/bin/$PYTHON_DIR/python3-config
-	ln -sf $VERSION_DIR/bin/python${PY_EXE}-config $VAR/$VERSION/bin/python3-config
-    ln -sf $VERSION_DIR/include/python$PY_EXE/* $VAR/$VERSION/include
+#	ln -sf $VERSION_DIR/bin/python${PY_EXE}-config $VAR/$VERSION/bin/$PYTHON_DIR/python3-config
+#	ln -sf $VERSION_DIR/bin/python${PY_EXE}-config $VAR/$VERSION/bin/python3-config
+#	ln -sf $VERSION_DIR/include/python$PY_EXE/* $VAR/$VERSION/lib
 
 	if [ ! -e /etc/ld.so.conf.d/gridlabd-$VERSION.conf ]; then
 		cd $HOME/temp
@@ -126,7 +126,7 @@ if [ ! -x $VERSION_DIR/bin/python3 -o "$($VERSION_DIR/bin/python3 --version | cu
 	fi
 
 	cd $VAR/$VERSION/bin
-    $VERSION_DIR/bin/python3.9 -m venv pkgenv
+    python3 -m venv pkgenv
 	$VAR/$VERSION/bin/$PYTHON_DIR/python3 -m pip install --upgrade pip
 	$VAR/$VERSION/bin/$PYTHON_DIR/python3 -m pip install matplotlib Pillow pandas numpy networkx pytz pysolar PyGithub scikit-learn xlrd boto3
 	$VAR/$VERSION/bin/$PYTHON_DIR/python3 -m pip install IPython censusdata
