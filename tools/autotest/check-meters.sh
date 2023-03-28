15f41abfd815978b2c4274826b65a3a061767f8b
Alyona Teyber <Ivanova.alyona5@gmail.com>
2023-03-28 12:59:41 -0700

    Fixing autotests

--- tools/autotest/check-meters.sh
+++ tools/autotest/check-meters.sh
@@ -2,6 +2,6 @@
 
 FAILED=0
 for MODEL_NAME in $*; do
-    diff --unified=0 -I'^//.*' ../${MODEL_NAME}-meters.glm ${MODEL_NAME}-meters.glm || FAILED=$(($FAILED+1))
+    diff --unified=0 -I '^\(|//\|.*guid.*\)' ../${MODEL_NAME}-meters.glm ${MODEL_NAME}-meters.glm || FAILED=$(($FAILED+1))
 done
 exit $FAILED
