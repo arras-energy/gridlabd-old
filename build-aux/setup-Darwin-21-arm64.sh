4ad76b2acacd38bcd91aa664584b07e42d2930c4
thistleman <codes5096@gmail.com>
2023-04-03 12:33:42 -0700

    potential m1 setup venv

--- build-aux/setup-Darwin-21-arm64.sh
+++ build-aux/setup-Darwin-21-arm64.sh
@@ -14,8 +14,9 @@
 # Set version and paths, using these vars will make future maintenance much better. #Automation
     VERSION=${VERSION:-`build-aux/version.sh --name`}
     VERSION_DIR=$VAR/$VERSION
-    PYTHON_DIR=pkgenv/bin
-    PYTHON_VER=3.9
+	PKG_PYTHON_DIR=/usr/local/bin
+	VENV_PYTHON_DIR=$VERSION_DIR/bin/pkgenv/bin
+    PYTHON_VER=3.9.6
     PY_EXE=3.9
 
 export PATH=$VERSION_DIR/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
@@ -71,46 +72,22 @@ export PATH=$VERSION_DIR/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
     [ ! -e /usr/local/bin/libtoolize ] && sudo ln -sf /opt/homebrew/bin/glibtoolize /usr/local/bin/libtoolize
     [ ! -e /usr/local/bin/libtool ] && sudo ln -sf /opt/homebrew/bin/glibtool /usr/local/bin/libtool
 
-export DYLD_LIBRARY_PATH=$VERSION_DIR/lib:$DYLD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$VERSION_DIR/lib:$LD_LIBRARY_PATH
-export LIBRARY_PATH=$VERSION_DIR/lib:$LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$VERSION_DIR/lib:$VERSION_DIR/include:$DYLD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$VERSION_DIR/lib:$VERSION_DIR/include:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$VERSION_DIR/lib:$VERSION_DIR/include:$LIBRARY_PATH
 
 # Install python $PYTHON_VER
 # python3 support needed as of 4.2
-if [ ! -x $VERSION_DIR/bin/python3 -o "$($VERSION_DIR/bin/python3 --version | cut -f3 -d.)" != "Python $PY_EXE" ]; then
-	echo "installing python $PYTHON_VER and creating .venv"
+if [ ! -x /usr/local/bin/python3 ] || [ "$(/usr/local/bin/python3 --version | cut -d' ' -f2 | cut -d. -f1-2)" != "$PY_EXE" ]; then
+	echo "installing python $PYTHON_VER"
     /opt/homebrew/bin/brew install python@$PYTHON_VER
 
-	cd $VERSION_DIR/bin
-    /opt/homebrew/bin/python3.9 -m venv pkgenv
-
-    sudo ln -sf /opt/homebrew/opt/python@$PY_EXE/Frameworks/Python.framework/Versions/$PY_EXE/bin/python$PY_EXE-config $VERSION_DIR/bin/$PYTHON_DIR/python3-config
-    sudo ln -sf /opt/homebrew/opt/python@$PY_EXE/Frameworks/Python.framework/Versions/$PY_EXE/bin/python$PY_EXE-config $VERSION_DIR/bin/python3-config
-    sudo ln -sf /opt/homebrew/opt/python@$PY_EXE/Frameworks/Python.framework/Versions/$PY_EXE/include/python$PY_EXE/* $VERSION_DIR/lib
-
-#	sudo ln -s $VERSION_DIR/$PYTHON_DIR/bin/python3 $VERSION_DIR/$PYTHON_DIR/bin/python3
-    $VERSION_DIR/bin/$PYTHON_DIR/python3 -m ensurepip --upgrade
-
-#	sudo ln -sf $VERSION_DIR/$PYTHON_DIR/bin/python${PY_EXE}d-config $VERSION_DIR/$PYTHON_DIR/bin/python3-config
-#	sudo ln -sf $VERSION_DIR/$PYTHON_DIR/bin/pydoc${PY_EXE} $VERSION_DIR/$PYTHON_DIR/bin/pydoc
-#	sudo ln -sf $VERSION_DIR/$PYTHON_DIR/bin/idle${PY_EXE} $VERSION_DIR/$PYTHON_DIR/bin/idle
-#    sudo ln -sf $VERSION_DIR/bin/$PYTHON_DIR/* $VERSION_DIR/bin
-
-    $VERSION_DIR/bin/$PYTHON_DIR/python3 -m ensurepip --upgrade
-	$VERSION_DIR/bin/$PYTHON_DIR/python3 -m pip install matplotlib Pillow pandas numpy networkx pytz pysolar PyGithub scikit-learn xlrd boto3
-    $VERSION_DIR/bin/$PYTHON_DIR/python3 -m pip install build
-    $VERSION_DIR/bin/$PYTHON_DIR/python3 -m pip install pyproj wheel
-
-#    sudo ln -sf $VERSION_DIR/$PYTHON_DIR/bin/* $VERSION_DIR/bin
-#    sudo ln -sf $VERSION_DIR/$PYTHON_DIR/include/* $VERSION_DIR/include
-#    sudo ln -sf $VERSION_DIR/$PYTHON_DIR/lib/* $VERSION_DIR/lib
-#    sudo ln -sf $VERSION_DIR/$PYTHON_DIR/share/* $VERSION_DIR/share
 fi
 
 # check for successful python build
-if [ ! -x $VERSION_DIR/bin/$PYTHON_DIR/python3 ]; then
+if [ ! -x /usr/local/bin/python3 ]; then
     echo "Could not locate python executable in"
-    echo "PYTHON LOCATION: $VERSION_DIR/bin/$PYTHON_DIR/python3"
+    echo "PYTHON LOCATION: /usr/local/bin/python3"
     echo "Exiting build."
     exit 1
 fi
@@ -148,12 +125,7 @@ fi
 
     ln -sf $VERSION_DIR/gridlabd/lib/libgeos* /usr/local/lib 
 
-# mysql connector
-#    brew install mysql
-#    brew install mysql-client
-
 sudo ln -sf /opt/homebrew/bin/* /usr/local/bin
-# sudo ln -sf /opt/homebrew/etc/* /usr/local/etc
 sudo ln -sf /opt/homebrew/lib/* /usr/local/lib
 
 cd /usr/local/bin
