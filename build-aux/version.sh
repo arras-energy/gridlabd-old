d43d1e5ac21d7b573953dcc2ded0d260936ba5ed
David P. Chassin <dchassin@slac.stanford.edu>
2023-04-18 15:21:33 -0700

    Update version.sh

--- build-aux/version.sh
+++ build-aux/version.sh
@@ -40,7 +40,7 @@ case $1 in
         if [ "$(uname -s)" == "Darwin" ]; then
             echo darwin$(uname -r | cut -f1 -d.)-$(uname -m)
         elif [ -f "/etc/os-release" ]; then
-            echo $(shell grep ^ID= /etc/os-release | cut -f2 -d= | tr -d '"')$(shell grep ^VERSION_ID= /etc/os-release | cut -f2 -d= | tr -d '"')
+            echo $(grep ^ID= /etc/os-release | cut -f2 -d= | tr -d '"')$(grep ^VERSION_ID= /etc/os-release | cut -f2 -d= | tr -d '"')
         else
             echo $(uname -s)$(uname -r)-$(uname -m)
         fi
