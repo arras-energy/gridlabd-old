d2d5832cc1ae9de9064ebfdf83f8036cdcd7825e
David P. Chassin <dchassin@slac.stanford.edu>
2023-04-19 08:15:29 -0700

    Update makefile to build requirements.txt

--- python/Makefile.mk
+++ python/Makefile.mk
@@ -3,12 +3,12 @@ PYTHONVERSION=$(shell python3 $(top_srcdir)/python/setup.py --version)
 $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION).tar.gz: $(top_srcdir)/source/build.h | $(PYENV)
 	@echo "building $@"
 	@rm -f $(top_srcdir)/python/dist/gridlabd-*.{whl,tar.gz}
-	@$(PYBIN)/python3 -m pip install build 1>/dev/null
-	@( export SRCDIR=$(realpath $(top_srcdir)) ; export BLDDIR=$(shell pwd); $(PYBIN)/python3 -m build $(top_srcdir)/python 1>/dev/null )
+	@$(PYACTIVATE);$(ENVPYTHON) -m pip install build 1>/dev/null
+	@( export SRCDIR=$(realpath $(top_srcdir)) ; export BLDDIR=$(shell pwd); $(PYACTIVATE);$(ENVPYTHON) -m build $(top_srcdir)/python 1>/dev/null )
 
 python-install: $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION).tar.gz
 	@echo "installing $@"
-	@$(PYBIN)/python3 -m pip install --ignore-installed $(top_srcdir)/python/dist/gridlabd-*.whl
+	@$(PYACTIVATE);$(ENVPYTHON) -m pip install --ignore-installed $(top_srcdir)/python/dist/gridlabd-*.whl
 
 python-clean:
 	@rm -f $(top_srcdir)/python/dist/*.{whl,tar.gz}
