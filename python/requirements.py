3f2de143995cdd0d3d2455c89fab31b09d7320b4
David P. Chassin <dchassin@slac.stanford.edu>
2023-04-20 11:20:46 -0700

    Move explicit pip installs to requirements.csv

--- python/requirements.py
+++ python/requirements.py
@@ -4,7 +4,7 @@ This script converts requirements.csv to requirements.txt by choosing the entrie
 that correspond to the current system information
 """
 
-import sys
+import sys, os
 import platform
 import pandas
 
@@ -15,22 +15,36 @@ else:
     sysinfo = "-".join([platform.system(),platform.release().split('.')[0],platform.machine()])
 csvfile = sys.argv[0].replace(".py",".csv")
 
+PRIORITY = False
+
 if len(sys.argv) == 2:
     if sys.argv[1] == "--sysinfo":
         print(sysinfo)
         exit(0)
+    elif sys.argv[1] == "--priority":
+        PRIORITY = True
     else:
         csvfile = sys.argv[1]
 
-requirements = pandas.DataFrame(pandas.read_csv(csvfile,index_col=['module']).stack().reset_index())
-requirements.columns = ["module","system","version"]
-requirements.set_index(["module","system"],inplace=True,verify_integrity=True)
-requirements.sort_index(inplace=True)
+requirements = pandas.DataFrame(pandas.read_csv(csvfile,index_col=['module']))
 
-for module in requirements.index.get_level_values(0).unique():
-    info = requirements.loc[module]
-    try:
+if not sysinfo in requirements.columns:
+    sysinfo = 'default'
+
+if PRIORITY:
+    priority = requirements[~requirements['priority'].isna()].reset_index().set_index(['priority','module']).sort_index()
+
+    for n,row in priority.reset_index().iterrows():
+        options = "" if str(row.options) == "nan" else row.options
+        print(f"{os.environ['_']} -m pip install {options} {row.module}=={row[sysinfo]}")
+
+else:
+    requirements = requirements[requirements['priority'].isna()].stack().reset_index()
+    requirements.columns = ["module","system","version"]
+    requirements.set_index(["module","system"],inplace=True,verify_integrity=True)
+    requirements.sort_index(inplace=True)
+
+    for module in requirements.index.get_level_values(0).unique():
+        info = requirements.loc[module]
         version = info.loc[sysinfo]['version']
-    except:
-        version = info.loc['default']['version']
-    print(f"{module}=={version}")
+        print(f"{module}=={version}")
