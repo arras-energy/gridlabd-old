d9d17b0b25c981a20a3e0f418f59a230ce39b3c1
David P. Chassin <dchassin@slac.stanford.edu>
2023-04-21 16:55:03 -0700

    Fix python-install

--- python/Makefile.mk
+++ python/Makefile.mk
@@ -1,17 +1,20 @@
 PYTHONVERSION=$(shell python3 $(top_srcdir)/python/setup.py --version)
+PYPKG=$(PYENV)/lib/python$(PYVER)/site-packages/gridlabd
 
-$(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION).tar.gz: $(top_srcdir)/source/build.h | $(PYENV)
+$(PYPKG)/gridlabd-$(PYTHONVERSION).tar.gz: $(top_srcdir)/source/build.h | $(PYENV)
 	@echo "building $@"
-	@rm -f $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION)-*.{whl,tar.gz}
-	@$(PYACTIVATE);$(ENVPYTHON) -m pip install build 1>/dev/null
-	@export SRCDIR=$(realpath $(top_srcdir)) ; export BLDDIR=$(shell pwd); $(PYACTIVATE); $(ENVPYTHON) -m build $(top_srcdir)/python 1>/dev/null
+	rm -f $(PYPKG)/gridlabd-$(PYTHONVERSION)-*.{whl,tar.gz}
+	$(PYACTIVATE);$(ENVPYTHON) -m pip install build 
+	export SRCDIR=$(realpath $(top_srcdir)) ; export BLDDIR=$(shell pwd); $(PYACTIVATE); $(ENVPYTHON) -m build $(top_srcdir)/python
+	mkdir -p $(PYPKG)
+	cp $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION)*.{tar.gz,whl} $(PYPKG)
 
-python-install: $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION).tar.gz
+python-install: $(PYPKG)/gridlabd-$(PYTHONVERSION).tar.gz
 	@echo "installing gridlabd-$(PYTHONVERSION)"
-	@$(PYACTIVATE);$(ENVPYTHON) -m pip install --ignore-installed $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION)-*.whl
+	@$(PYACTIVATE);$(ENVPYTHON) -m pip install --ignore-installed $(PYPKG)/gridlabd-$(PYTHONVERSION)-*.whl
 
 python-clean:
-	@rm -f $(top_srcdir)/python/dist/*.{whl,tar.gz}
+	@rm -f $(PYPKG)/gridlabd-*.{whl,tar.gz}
 
 python-uninstall:
 	@echo "uninstalling $(top_srcdir)/python"
