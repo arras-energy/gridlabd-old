d3b806b05364d0708ccc441aaaa50d2c215b45d4
David P. Chassin <david.chassin@me.com>
2023-04-22 08:15:08 -0700

    Update Makefile.mk

--- python/Makefile.mk
+++ python/Makefile.mk
@@ -3,25 +3,25 @@ PYPKG=$(PYENV)/lib/python$(PYVER)/site-packages/gridlabd
 
 $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION).tar.gz: $(top_srcdir)/source/build.h | $(PYENV)
 	@echo "building $@"
-	rm -f $(PYPKG)/gridlabd-$(PYTHONVERSION)*.{whl,tar.gz}
-	$(ENVPYTHON) -m pip install build 
-	export SRCDIR=$(realpath $(top_srcdir)) ; export BLDDIR=$(shell pwd); $(ENVPYTHON) -m build $(top_srcdir)/python
+	@rm -f $(PYPKG)-$(PYTHONVERSION)*.{whl,tar.gz}
+	@$(ENVPYTHON) -m pip install build 
+	@export SRCDIR=$(realpath $(top_srcdir)) ; export BLDDIR=$(shell pwd); $(ENVPYTHON) -m build $(top_srcdir)/python
 
-$(PYPKG)/gridlabd-$(PYTHONVERSION).tar.gz: $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION).tar.gz
-	mkdir -p $(PYPKG)
-	cp $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION)*.{tar.gz,whl} $(PYPKG)
-	ls -lR $(PYPKG)
+$(PYPKG)-$(PYTHONVERSION).tar.gz: $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION).tar.gz
+	@cp $(top_srcdir)/python/dist/gridlabd-$(PYTHONVERSION)*.{tar.gz,whl} $(PYENV)/lib/python$(PYVER)/site-packages
+
+python-install: $(PYPKG)-$(PYTHONVERSION).tar.gz
+	@echo "installing gridlabd-$(PYTHONVERSION)"
+	$(ENVPYTHON) -m pip install --ignore-installed $(PYPKG)-$(PYTHONVERSION)-*.whl
+	ls -lR $(PYPKG)*
 	$(ENVPYTHON) -c "import gridlabd; print(gridlabd.__file__)"
 
-python-install: $(PYPKG)/gridlabd-$(PYTHONVERSION).tar.gz
-	@echo "installing gridlabd-$(PYTHONVERSION)"
-	$(ENVPYTHON) -m pip install --ignore-installed $(PYPKG)/gridlabd-$(PYTHONVERSION)-*.whl
-
 python-clean:
-	@rm -f $(PYPKG)/gridlabd-*.{whl,tar.gz}
+	@echo "removing intermediate python module build artifacts"
+	@rm -rf $(top_srcdir)/python/dist
 
 python-uninstall:
-	@echo "uninstalling $(top_srcdir)/python"
-	@rm -rf $(PYENV)
+	@echo "removing $(PYPKG) module files"
+	@rm -rf $(PYPKG)*
 
 BUILT_SOURCES += python-install
