eff5e6500e1e006639504d6a4544340d2312618b
David P. Chassin <david.chassin@me.com>
2023-04-23 07:06:24 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 curl --version 1>/dev/null 2>&1 || (echo "ERROR: you must install curl first"; exit 1)
 export INSTALL_SYSTEM=$(uname -s)
-(curl -fsL https://raw.githubusercontent.com/dchassin/gridlabd/develop-fix-install/setup/$INSTALL_SYSTEM.sh || (echo "ERROR: unable to find installer for $INSTALL_SYSTEM. Manual install required." >/dev/stderr; exit 1)) | /bin/sh
+export GRIDLABD_SOURCE=https://raw.githubusercontent.com/${GRIDLABD_ORG:-slacgismo}/${GRIDLABD_REPO:-gridlabd}/${GRIDLABD_BRANCH:-master}
+(curl -fsL $GRIDLABD_SOURCE/setup/$INSTALL_SYSTEM.sh || (echo "ERROR: unable to find $GRIDLABD_SOURCE/setup/$INSTALL_SYSTEM.sh. Manual install required." >/dev/stderr; exit 1)) | /bin/sh
