8fe4f2a5237fcd5ce88f4b2d51006cd01e53f936
David P. Chassin <david.chassin@me.com>
2023-04-23 08:13:10 -0700

    Update Linux-ubuntu-20.sh

--- setup/Linux-ubuntu-20.sh
+++ setup/Linux-ubuntu-20.sh
@@ -2,14 +2,19 @@
 
 PYTHON_VERSION=3.10
 
-sudo apt update
-sudo apt upgrade -y
+if ! "$(whoami)" == "root"; then
+	echo "ERROR: you must run setup as root or use sudo"
+	exit 1
+fi
+
+apt update
+apt upgrade -y
 
 if "$(which  python$PYTHON_VERSION)" == "" ; then
 	echo "Installing python${PYTHON_VERSION}..."
-	sudo apt install software-properties-common -y
-	sudo add-app-repository ppa:deadsnakes/ppa
-	sudo apt install python$PYTHON_VERSION -qqqqy
+	apt install software-properties-common -y
+	add-app-repository ppa:deadsnakes/ppa
+	apt install python$PYTHON_VERSION -qqqqy
 	exit 1
 fi
 
