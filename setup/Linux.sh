5d2a0a32a5434f6daeb831299573029fe3e685c5
David P. Chassin <david.chassin@me.com>
2023-04-23 07:26:53 -0700

    Update Linux.sh

--- setup/Linux.sh
+++ setup/Linux.sh
@@ -1,2 +1,2 @@
-export INSTALL_VERSION=$(uname -r | cut -f1 -d.)
-(curl -fsL $GRIDLABD_SOURCE/setup/Linux-$INSTALL_VERSION.sh || (echo "ERROR: unable to find $GRIDLABD_SOURCE/setup/Linux-$INSTALL_VERSION.sh. Manual install required." >/dev/stderr; exit 1)) | /bin/sh
+. /etc/os-release
+(curl -fsL $GRIDLABD_SOURCE/setup/Linux-$ID-${VERSION_ID%%.*}.sh || (echo "ERROR: unable to find $GRIDLABD_SOURCE/setup/Linux-$ID-${VERSION_ID%%.*}.sh. Manual install required." >/dev/stderr; exit 1)) | /bin/sh
