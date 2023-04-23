ce9f66991a22b6a5deb1315135cd7b04c4801cda
David P. Chassin <david.chassin@me.com>
2023-04-23 07:06:27 -0700

    Update Darwin.sh

--- setup/Darwin.sh
+++ setup/Darwin.sh
@@ -1,2 +1,2 @@
 export INSTALL_VERSION=$(uname -r | cut -f1 -d.)
-(curl -fsL https://raw.githubusercontent.com/dchassin/gridlabd/develop-fix-install/setup/Darwin-$INSTALL_VERSION.sh || (echo "ERROR: unable to find installer for Darwin $INSTALL_VERSION. Manual install required." >/dev/stderr; exit 1)) | /bin/sh
+(curl -fsL $GRIDLABD_SOURCE/setup/Darwin-$INSTALL_VERSION.sh || (echo "ERROR: unable to find $GRIDLABD_SOURCE/setup/Darwin-$INSTALL_VERSION.sh. Manual install required." >/dev/stderr; exit 1)) | /bin/sh
