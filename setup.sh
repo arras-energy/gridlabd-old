1f283d350957270f352e4659041cf80c24ccd482
David P. Chassin <dchassin@slac.stanford.edu>
2023-04-24 08:49:14 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
-curl --version 1>/dev/null 2>&1 || (echo "ERROR: you must install curl first"; exit 1)
-export SYSTEMNAME=$(uname -s)
-export GRIDLABD_SOURCE=https://raw.githubusercontent.com/${GRIDLABD_ORG:-slacgismo}/${GRIDLABD_REPO:-gridlabd}/${GRIDLABD_BRANCH:-master}
-(curl -fsL $GRIDLABD_SOURCE/setup/$SYSTEMNAME.sh || (echo "ERROR: unable to find $GRIDLABD_SOURCE/setup/$SYSTEMNAME.sh. Manual setup required (see $GRIDLABD_SOURCE/setup/README.md)." >/dev/stderr; exit 1)) | /bin/sh
+set +x
+
+if curl --version 1>/dev/null 2>&1 ; then
+	export SYSTEMNAME=$(uname -s)
+	export GRIDLABD_SOURCE=https://raw.githubusercontent.com/${GRIDLABD_ORG:-slacgismo}/${GRIDLABD_REPO:-gridlabd}/${GRIDLABD_BRANCH:-master}
+	if ! curl -sL $GRIDLABD_SOURCE/setup/$SYSTEMNAME.sh > /tmp/setup_$$.sh ; then
+		sh /tmp/setup_$$.sh
+		rm /tmp/setup_$$.sh
+	else
+		echo "ERROR: unable to find $GRIDLABD_SOURCE/setup/$SYSTEMNAME.sh. Manual setup required." >/dev/stderr
+	fi		
+else
+	echo "ERROR: you must install curl first" > /dev/stderr
+fi
