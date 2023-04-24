9d97c7654084a5b896ca56811e503f55aee5385d
David P. Chassin <dchassin@slac.stanford.edu>
2023-04-24 07:39:07 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 curl --version 1>/dev/null 2>&1 || (echo "ERROR: you must install curl first"; exit 1)
-export INSTALL_SYSTEM=$(uname -s)
+export SYSTEMNAME=$(uname -s)
 export GRIDLABD_SOURCE=https://raw.githubusercontent.com/${GRIDLABD_ORG:-slacgismo}/${GRIDLABD_REPO:-gridlabd}/${GRIDLABD_BRANCH:-master}
-(curl -fsL $GRIDLABD_SOURCE/setup/$INSTALL_SYSTEM.sh || (echo "ERROR: unable to find $GRIDLABD_SOURCE/setup/$INSTALL_SYSTEM.sh. Manual install required." >/dev/stderr; exit 1)) | /bin/sh
+(curl -fsL $GRIDLABD_SOURCE/setup/$SYSTEMNAME.sh || (echo "ERROR: unable to find $GRIDLABD_SOURCE/setup/$SYSTEMNAME.sh. Manual setup required (see $GRIDLABD_SOURCE/setup/README.md)." >/dev/stderr; exit 1)) | /bin/sh
