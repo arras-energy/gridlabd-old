c1c13d0b7e612efca09eb68121191df60d52b043
David P. Chassin <david.chassin@me.com>
2023-04-23 19:33:17 -0700

    Add install folder

---
+++ install/ubuntu-22.sh
@@ -0,0 +1,21 @@
+apt update 
+apt install curl -y
+
+export GRIDLABD_ORG=dchassin
+export GRIDLABD_REPO=gridlabd
+export GRIDLABD_BRANCH=develop-fix-install
+
+curl -sL https://raw.githubusercontent.com/${GRIDLABD_ORG}/${GRIDLABD_REPO}/${GRIDLABD_BRANCH}/setup.sh | sh
+
+apt install git -y
+git clone https://github.com/$GRIDLABD_ORG/$GRIDLABD_REPO -b $GRIDLABD_BRANCH --depth 1 gridlabd
+
+cd /gridlabd
+. $HOME/.venv/gridlabd/bin/activate
+autoreconf -isf
+./configure
+make -j$(($(nproc)*3)) system
+gridlabd -T 0 --validate
+
+echo "Starting interactive shell. Use Ctrl-D to exit."
+bash
