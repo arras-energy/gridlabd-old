2481a0e966e559e8d683c645ddb34b980af1889c
David P. Chassin <dchassin@slac.stanford.edu>
2023-04-28 12:41:32 -0700

    Update setup-dev.sh

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,8 +1,8 @@
 export GRIDLABD_ORIGIN=dchassin/gridlabd/develop-fix-install
 apt-get update
-apt-get upgrade -y
 apt-get install curl -y
+curl -fsL https://raw.githubusercontent.com/$GRIDLABD_ORIGIN/setup.sh | sh
+apt-get upgrade -y
 apt-get install git -y
 test -d /gridlabd/.git || git clone https://github.com/${GRIDLABD_ORIGIN%/*} -b ${GRIDLABD_ORIGIN##*/} --depth 1 /gridlabd
 cd /gridlabd
-. setup.sh
