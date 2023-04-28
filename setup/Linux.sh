9d69274bf6661e9ae9977d7eca8d29d3c3c4ff7c
David P. Chassin <dchassin@slac.stanford.edu>
2023-04-28 11:34:51 -0700

    Update Linux.sh

--- setup/Linux.sh
+++ setup/Linux.sh
@@ -1,3 +1,3 @@
 . /etc/os-release
-SOURCE=$GRIDLABD_ORIGIN/setup/$ID-${VERSION_ID%%.*}.sh
+SOURCE=$GRIDLABD_ORIGIN/setup/$ID-${VERSION_ID%%.*}-${uname -m}.sh
 (curl -H 'Cache-Control: no-cache' -fsL $SOURCE || (echo "ERROR: unable to find $SOURCE. Manual install required." >/dev/stderr; exit 1)) | /bin/sh
