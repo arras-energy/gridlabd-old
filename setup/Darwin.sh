b760d7226c340ca0667d6c8863cd41ccfc3b6be4
David P. Chassin <dchassin@slac.stanford.edu>
2023-04-28 12:15:39 -0700

    Update Darwin.sh

--- setup/Darwin.sh
+++ setup/Darwin.sh
@@ -1,2 +1,4 @@
-export INSTALL_VERSION=$(uname -r | cut -f1 -d.)
-(curl -H 'Cache-Control: no-cache' -fsL $GRIDLABD_ORIGIN/setup/darwin_$INSTALL_VERSION.sh || (echo "ERROR: unable to find $GRIDLABD_SOURCE/setup/Darwin-$INSTALL_VERSION.sh. Manual install required." >/dev/stderr; exit 1)) | /bin/sh
+export ID="darwin"
+export VERSION_ID=$(uname -r | cut -f1 -d.)
+SOURCE=$GRIDLABD_ORIGIN/setup/${ID}_${VERSION_ID%%.*}-$(uname -m).sh
+(curl -H 'Cache-Control: no-cache' -fsL $SOURCE || (echo "ERROR: unable to find $SOURCE. Manual install required." >/dev/stderr; exit 1)) | /bin/sh
