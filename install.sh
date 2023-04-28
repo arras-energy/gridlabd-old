cf4f4284fcf93e01c31b5c7c79cd119d4b3a7c28
David P. Chassin <dchassin@slac.stanford.edu>
2023-04-28 16:22:18 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,12 +21,12 @@ fi
 if [ ! -z "${TARGET}" ]; then
 	mkdir -p $OPT/gridlabd
 	cd $OPT/gridlabd
-	if ! (curl -sL https://install.gridlabd.us/$1.tarz | tar xz) ; then
+	if ! (curl -sL https://install.gridlabd.us/$TARGET.tarz | tar xz) ; then
 		echo "ERROR: unable to find install image for $TARGET" > /dev/stderr
-	elif ! sh $1/share/gridlabd/setup.sh ; then
+	elif ! sh $TARGET/share/gridlabd/setup.sh ; then
 		echo "ERROR: setup script not found for $TARGET" > /dev/stderr
 	else
-		ln -sf $1 current
+		ln -sf $TARGET current
 		ln -sF $OPT/current/bin/gridlabd /usr/local/bin/gridlabd
 		if [ ! "$(/usr/local/bin/gridlabd --version=name)" == "$TARGET" ] ; then
 			echo "ERROR: /usr/local/bin/gridlabd not linked to $TARGET > /dev/stderr
