83340390d14217ccac4f76400963546cb1bbd7bb
David P. Chassin <david.chassin@me.com>
2023-04-29 11:10:26 -0700

    Create ubuntu_20.sh

---
+++ install/ubuntu_20.sh
@@ -0,0 +1,17 @@
+apt update 
+apt install curl -y
+. /etc/os-release
+
+LATEST=$(curl -sL http://install.gridlabd.us/master.txt)
+
+curl -sL https://install.gridlabd.us/
+
+apt-get install git -y
+git clone https://github.com/$GRIDLABD_ORG/$GRIDLABD_REPO -b $GRIDLABD_BRANCH --depth 1 gridlabd
+
+cd /gridlabd
+. $HOME/.venv/gridlabd/bin/activate
+autoreconf -isf
+./configure
+make -j$(($(nproc)*3)) system
+gridlabd -T 0 --validate
