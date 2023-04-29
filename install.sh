039a57641b02b8193794b34d009b8a15ba474ed5
David P. Chassin <david.chassin@me.com>
2023-04-29 07:21:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 set -x
-
-OPT=/usr/local/opt
+INSTALL_SOURCE="http://install.gridlabd.us"
+OPT="/usr/local/opt"
 
 if [ $# -eq 0 ]; then
 	case $(uname -s) in
@@ -19,15 +19,15 @@ fi
 if [ -z "${GRIDLABD_IMAGE}" ]; then
 	echo "ERROR: GRIDLABD_IMAGE name not specified" > /dev/stderr
 else
-	mkdir -p $OPT/gridlabd
-	cd $OPT/gridlabd
-	if ! (curl -sL https://install.gridlabd.us/$GRIDLABD_IMAGE.tarz | tar xz) ; then
+	mkdir -p "$OPT/gridlabd"
+	cd "$OPT/gridlabd"
+	if ! (curl -sL "$INSTALL_SOURCE/$GRIDLABD_IMAGE.tarz" | tar xz) ; then
 		echo "ERROR: unable to find install image for $TARGET" > /dev/stderr
-	elif ! sh $GRIDLABD_IMAGE/share/gridlabd/setup.sh ; then
+	elif ! sh "$GRIDLABD_IMAGE/share/gridlabd/setup.sh" ; then
 		echo "ERROR: setup script not found for $GRIDLABD_IMAGE" > /dev/stderr
 	else
-		ln -sf $GRIDLABD_IMAGE current
-		ln -sF $OPT/current/bin/gridlabd /usr/local/bin/gridlabd
+		ln -sf "$GRIDLABD_IMAGE" "current"
+		ln -sF "$OPT/current/bin/gridlabd" "/usr/local/bin/gridlabd"
 		if [ ! "$(/usr/local/bin/gridlabd --version=name)" == "$GRIDLABD_IMAGE" ] ; then
 			echo "ERROR: /usr/local/bin/gridlabd not linked to $GRIDLABD_IMAGE" > /dev/stderr
 		fi
