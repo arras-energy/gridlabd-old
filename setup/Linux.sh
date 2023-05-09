1fc61f2fd7554c293c4914db2a5c4dce7b6fa91a
David P. Chassin <dchassin@slac.stanford.edu>
2023-05-09 12:26:31 -0700

    Added ability use common setup scripts

--- setup/Linux.sh
+++ setup/Linux.sh
@@ -1,8 +1,13 @@
 error () { echo "ERROR [Linux.sh]: $*" > /dev/stderr ; exit 1; }
 . /etc/os-release
+COMMON=$GRIDLABD_ORIGIN/setup/${ID}_${VERSION_ID%%.*}.sh
 SOURCE=$GRIDLABD_ORIGIN/setup/${ID}_${VERSION_ID%%.*}-$(uname -m).sh
 if [ "$GRIDLABD_ORIGIN" = "." ]; then
-	sh $SOURCE || error "unable to run $GRIDLABD_ORIGIN"
-else
+	if [ -f $COMMON ]; then
+		sh $COMMON || error "unable to setup from $GRIDLABD_ORIGIN"
+	else
+		sh $SOURCE || error "unable to setup from $GRIDLABD_ORIGIN"
+	fi
+elif ! (curl -H 'Cache-Control: no-cache' -fsL $COMMON | sh); then
 	( curl -H 'Cache-Control: no-cache' -fsL $SOURCE | sh ) || error "unable to find $SOURCE. Manual install required."=
 fi
